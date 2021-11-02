eb98528247c386fe911ecd4490084997471363f9
hunter <iamhunter32@gmail.com>
2021-11-02 15:35:34 -0400

    renamed pack for less confusion in imports

--- Core/Animals/Base/__init__.py
+++
@@ -1,6 +0,0 @@
-from Core.Animals.Base.tier_1 import *
-from Core.Animals.Base.tier_2 import *
-from Core.Animals.Base.tier_3 import *
-from Core.Animals.Base.tier_4 import *
-from Core.Animals.Base.tier_5 import *
-from Core.Animals.Base.tier_6 import *
--- Core/Animals/Paid_1/__init__.py
+++
@@ -1,7 +0,0 @@
-from Core.Animals.Paid_1.tier_1 import *
-from Core.Animals.Paid_1.tier_2 import *
-from Core.Animals.Paid_1.tier_3 import *
-from Core.Animals.Paid_1.tier_4 import *
-from Core.Animals.Paid_1.tier_5 import *
-from Core.Animals.Paid_1.tier_6 import *
-
