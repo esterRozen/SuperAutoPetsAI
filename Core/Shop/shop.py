c75ab385f46fcab3dd0c06788ac904fe17e2003e
hunter <iamhunter32@gmail.com>
2021-11-02 15:37:06 -0400

    started work on the shop

--- Core/Shop/shop.py
+++ Core/Shop/shop.py
@@ -1,3 +1,6 @@
+from Core.Animals.spawner import Spawner
+
+
 class Shop:
     tier = 1
 
@@ -7,10 +10,8 @@ class Shop:
     anim = []
     food = []
 
-    anim_frozen = []
-    food_frozen = []
-
-    def __init__(self):
+    def __init__(self, mode):
+        self.spawner = Spawner(mode)
         # populate with 3 tier 1 animals and 1 tier 1 food
         # mark all unfrozen
         pass
@@ -23,6 +24,14 @@ class Shop:
     def spawn(self):
         pass
 
+    def item(self, position):
+        if position >= self.size_animals + self.size_food or position < 0:
+            return 0
+        if position >= self.size_animals:
+            position -= self.size_animals
+            return self.food[position]
+        return self.anim[position]
+
     def rank_up(self):
         # increase rank, adjust max size accordingly
         pass
@@ -31,6 +40,15 @@ class Shop:
         # populate shop based on current rank and animals frozen
         pass
 
-    def freeze(self):
-        # toggle whether an animal can be rerolled
+    @staticmethod
+    def freeze(item):
+        item.freeze = not item.freeze
         pass
+
+
+class ShopItem:
+    frozen = False
+
+    def __init__(self, item, cost):
+        self.item = item
+        self.cost = cost
