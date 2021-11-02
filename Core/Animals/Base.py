9f13c2d7e8f812b1d400ea6ae527a68bb78d450c
hunter <iamhunter32@gmail.com>
2021-11-02 15:36:35 -0400

    created a pack bundle to provide a list of all pack's class objects sorted by tiers

---
+++ Core/Animals/Base.py
@@ -0,0 +1,27 @@
+import sys, inspect
+from Core.Animals.Base_Pack import *
+
+
+# meat and potatoes of it
+# this will create a list of all different animal objects, sorted into sublists
+# according to their respective tiers
+class Base:
+    def __init__(self):
+        self.animals = [[],[],[],[],[],[]]
+        for name, obj in inspect.getmembers(sys.modules[__name__]):
+            if inspect.isclass(obj):
+                class_name = inspect.getmodule(obj).__name__
+                if class_name.startswith('Core.Animals.BasePack'):
+                    if class_name.endswith("tier_1"):
+                        self.animals[0] += [obj()]
+                    elif class_name.endswith("tier_2"):
+                        self.animals[1] += [obj()]
+                    elif class_name.endswith("tier_3"):
+                        self.animals[2] += [obj()]
+                    elif class_name.endswith("tier_4"):
+                        self.animals[3] += [obj()]
+                    elif class_name.endswith("tier_5"):
+                        self.animals[4] += [obj()]
+                    elif class_name.endswith("tier_6"):
+                        self.animals[5] += [obj()]
+
