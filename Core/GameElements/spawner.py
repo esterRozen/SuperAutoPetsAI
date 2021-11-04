efd875d70b2ceba04a7e9435ec8cdad3fb030749
hunter <iamhunter32@gmail.com>
2021-11-03 23:43:25 -0400

    bug fixes and privatizing Spawner variables

--- Core/GameElements/spawner.py
+++ Core/GameElements/spawner.py
@@ -7,32 +7,38 @@ import random
 # outputs a random animal given a range of tiers
 class Spawner:
     def __init__(self, mode):
-        self.population = []
+        self._population = []
         if mode == "base":
-            self.population = Base().animals
+            self._population = Base().animals
         elif mode == "paid_1":
-            self.population = Paid1().animals
+            self._population = Paid1().animals
         elif mode == "food":
-            self.population = Items().items
+            self._population = Items().items
 
     def spawn(self, max_tier):
-        return self.spawn_n(1, max_tier)[0]
+        return type(self.spawn_n(1, max_tier)[0])()
 
     def spawn_n(self, n, max_tier):
-        max_idx = max(max_tier, 6)
-        max_idx = min(max_idx, 1)
+        max_idx = min(max_tier, 6)
+        max_idx = max(max_idx, 1)
 
-        subpopulation = sum(self.population[:max_idx])
-        return random.choices(subpopulation, k=n)
+        subpopulation = sum(self._population[:max_idx], [])
+        sample = random.choices(subpopulation, k=n)
+        return [type(instance)() for instance in sample]
 
     def spawn_tier(self, tier):
         idx = tier - 1
-        idx = min(idx, 0)
-        idx = max(idx, 5)
+        idx = max(idx, 0)
+        idx = min(idx, 5)
 
-        subpopulation = self.population[idx]
-        return random.choice(subpopulation)
+        subpopulation = self._population[idx]
+        return type(random.choice(subpopulation))()
 
 
 if __name__ == "__main__":
     spawner = Spawner("base")
+
+    spawner.spawn(2)
+    spawner.spawn(3)
+    spawner.spawn_n(3,2)
+    print("end")
