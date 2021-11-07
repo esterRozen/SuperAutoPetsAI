10c1edd620ad301812d944b11931c465ec540c14
hunter <iamhunter32@gmail.com>
2021-11-07 02:55:35 -0500

    added tier to Animal, added comparison to Animal for battle system, removed Roster

--- Core/GameElements/simpleClasses.py
+++ Core/GameElements/simpleClasses.py
@@ -5,6 +5,8 @@ class Animal:
     battle_atk = 0
     cost = 3
 
+    tier = 0
+
     def __init__(self, atk, hp):
         self.hp = hp
         self.atk = atk
@@ -31,6 +33,17 @@ class Animal:
     def trigger(self, name):
         return 0
 
+    def __lt__(self, other):
+        if self.battle_atk < other.battle_atk:
+            return True
+        elif self.battle_atk == other.battle_atk:
+            if self.battle_hp < other.battle_hp:
+                return True
+            elif self.battle_hp == other.battle_hp:
+                if self.tier < other.tier:
+                    return True
+        return False
+
 
 # noinspection PyUnusedLocal,PyMethodMayBeStatic
 class Equipment:
@@ -38,78 +51,3 @@ class Equipment:
 
     def trigger(self, name):
         return 0
-
-
-class Roster:
-    def __init__(self, size):
-        self._contents = [None] * size
-        self.max_size = size
-
-    def item(self, position):
-        return self._contents[position]
-
-    def remove(self, position):
-        item = self._contents[position]
-        self._contents[position] = None
-        return item
-
-    def clear(self):
-        self._contents = [None] * self.max_size
-
-    def grow(self):
-        self.max_size += 1
-
-    def add(self, item):
-        """
-
-        :param item: item to insert
-        :return: position inserted to, 0 on failure
-        """
-        i = 0
-        while i < self.max_size:
-            if self._contents[i] is None:
-                self._contents[i] = item
-                return i
-        return -1
-
-    def add_at_pos(self, item, position):
-        """
-        inserts to the position, default move right unless there's no space
-        :param item:
-        :param position:
-        :return: -1 on failure
-        """
-        if self._contents.count(None) == 0:
-            return -1
-
-        if self._contents[position] is None:
-            pass
-        # if you need to, shift stuff to the right.
-        elif self._contents[position:].count(None) != 0:
-            value = self._contents[position]
-            self._shift_right_recurse(position, value)
-        elif self._contents[:position].count(None) != 0:
-            value = self._contents[position]
-            self._shift_left_recurse(position, value)
-
-        self._contents[position] = item
-        return 1
-
-    def _shift_left_recurse(self, position, value):
-        if self._contents[position - 1] is not None:
-            nextValue = self._contents[position - 1]
-            self._shift_left_recurse(position - 1, nextValue)
-        self._contents[position - 1] = value
-
-    def _shift_right_recurse(self, position, value):
-        """
-        assumes that at some point there is an empty space in
-        self.contents
-        :param position:
-        :param value:
-        :return:
-        """
-        if self._contents[position + 1] is not None:
-            nextValue = self._contents[position + 1]
-            self._shift_right_recurse(position + 1, nextValue)
-        self._contents[position + 1] = value
