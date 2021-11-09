7d1cfb91e73d3c242a0d41487e01c1a1e2b31b88
hunter <iamhunter32@gmail.com>
2021-11-08 19:38:16 -0500

    added helper functions to team structure

--- Core/Team/team.py
+++ Core/Team/team.py
@@ -9,10 +9,67 @@ class Team:
         self.animals = roster
         self.equipment = equipment
         self.acting = 0
+        # random.seed(1)
+
+    def size(self):
+        i = 0
+        for animal in self.animals:
+            if not isinstance(animal, Empty):
+                i += 1
+        return i
+
+    def ret_diff_tiers(self):
+        pass
 
     def level(self):
         # level of currently acting team member
         return self.animals[self.acting].level()
+
+    def has_lvl3(self):
+        for animal in self.animals:
+            if animal.level() == 3:
+                return True
+        return False
+
+    def leftmost_unit(self):
+        i = 0
+        while isinstance(self.animals[i], Empty):
+            i += 1
+        return self.animals[i]
+
+    def friend_ahead(self):
+        for j in range(self.acting, len(self.animals)):
+            if isinstance(self.animals[j], Empty):
+                return self.animals[j]
+
+    def friends_ahead(self, n):
+        ret = []
+        i = 0
+        for j in range(self.acting, len(self.animals)):
+            if isinstance(self.animals[j], Empty):
+                if i < n:
+                    ret += [self.animals[j]]
+                    i += 1
+        if len(ret) == 1:
+            return ret[0]
+        return ret
+
+    def friend_behind(self):
+        for j in range(self.acting-1, -1, -1):
+            if isinstance(self.animals[j], Empty):
+                return self.animals[j]
+
+    def friends_behind(self, n):
+        ret = []
+        i = 0
+        for j in range(self.acting-1, -1, -1):
+            if isinstance(self.animals[j], Empty):
+                if i < n:
+                    ret += [self.animals[j]]
+                    i += 1
+        if len(ret) == 1:
+            return ret[0]
+        return ret
 
     def random_friend(self):
         a: List[int] = list(range(0, 5))
