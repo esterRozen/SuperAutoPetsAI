2db9aaa2fb954d4ad55c79f98924a96793157c36
hunter <iamhunter32@gmail.com>
2021-11-08 19:36:18 -0500

    fixed bad inheritance

--- Core/Overseer/BaseHandler.py
+++ Core/Overseer/BaseHandler.py
@@ -1,5 +1,5 @@
 class BaseHandler:
-    def load(self, team, turn, gold=10, shop=None):
+    def load(self, team, turn, gold=10, shop=None, hearts=4, battle_lost=False):
         pass
 
     def send_engine_message(self, message):
