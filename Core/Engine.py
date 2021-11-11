84b868984a90bdfe3fde53b89994fef516f6910f
hunter <iamhunter32@gmail.com>
2021-11-10 23:08:13 -0500

    created Engine class that will take user input and direct game

--- Core/Engine.py
+++ Core/Engine.py
@@ -1,1 +1,28 @@
 # must have a way to save and load states
+from Core.Overseer import MessageHandler
+
+
+class Engine:
+    def __init__(self, mode):
+        self.system = MessageHandler(mode)
+
+    def move_action(self, roster_init, roster_final):
+        pass
+
+    def combine_action(self, roster_init, roster_final):
+        pass
+
+    def sell_action(self, unit):
+        pass
+
+    def buy_action(self, shop_init, roster_final):
+        pass
+
+    def freeze_action(self, shop_pos):
+        pass
+
+    def reroll_action(self):
+        pass
+
+    def end_turn_action(self):
+        pass
