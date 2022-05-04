b575f1934c64a9232d83dbabc7dbba3abb787009
hunter <iamhunter32@gmail.com>
2022-05-04 15:44:09 -0400

    completed spawner testing

--- tests/Core/GameElements/AbstractElements/test_Spawner.py
+++ tests/Core/GameElements/AbstractElements/test_Spawner.py
@@ -1,23 +1,48 @@
 from unittest import TestCase
-import src.Core.GameElements.AbstractElements as ae
+from typing import List
+from src.Core.GameElements.AbstractElements import Spawner, Animal, Equipment
 
 
 class TestSpawner(TestCase):
     def test_base(self):
-        self.fail()
+        spawner = Spawner("base")
+        for _ in range(10):
+            animal: type(Animal) = spawner.spawn_tier(3)
+            self.assertTrue(animal.tier == 3, "wrong tier")
 
     def test_equipment(self):
-        self.fail()
+        spawner = Spawner("food")
+        for _ in range(10):
+            item: type(Equipment) = spawner.spawn_tier(2)
+            self.assertTrue(item.tier == 2, "wrong tier")
 
     def test_paid1(self):
-        self.fail()
+        spawner = Spawner("paid_1")
+        for _ in range(10):
+            animal: type(Animal) = spawner.spawn_tier(4)
+            self.assertTrue(animal.tier == 4, "wrong tier")
 
     def test_spawn(self):
-        spawner = ae.Spawner("base")
-        self.fail()
+        spawner = Spawner("base")
+        for _ in range(20):
+            animal: type(Animal) = spawner.spawn(4)
+            self.assertTrue(animal.tier <= 4, "wrong tier")
+            self.assertIsInstance(animal, Animal, "not an animal")
+            self.assertTrue(animal.cost == 3, "wrong cost")
 
     def test_spawn_n(self):
-        self.fail()
+        spawner = Spawner("paid_1")
+        for _ in range(10):
+            animals: List[type(Animal)] = spawner.spawn_n(5, 3)
+            self.assertTrue(len(animals) == 5, "wrong number of animals")
+            for animal in animals:
+                self.assertTrue(animal.tier <= 3, "wrong tier")
 
     def test_spawn_tier(self):
-        self.fail()
+        spawner = Spawner("base")
+        for _ in range(20):
+            animal: type(Animal) = spawner.spawn_tier(4)
+            self.assertTrue(animal.tier == 4, "wrong tier")
+        for i in range(0, 8):
+            animal: type(Animal) = spawner.spawn_tier(i)
+            self.assertTrue(max(min(animal.tier, 6), 1) == max(min(i, 6), 1), "wrong tier")
