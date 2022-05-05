767a875a8a13b93dfdc8e1133f75c322bb854544
hunter <iamhunter32@gmail.com>
2022-05-05 17:47:58 -0400

    test functions for presence of proper animals in each pack

--- tests/Core/GameElements/Objects/test_Food.py
+++ tests/Core/GameElements/Objects/test_Food.py
@@ -1,6 +1,27 @@
 from unittest import TestCase
+from src.Core.GameElements.Objects import Items
 
 
 class TestItems(TestCase):
+    base_food = [
+        [
+            'Apple', 'Honey'
+        ], [
+            'Cupcake', 'MeatBone', 'SleepingPill', 'Weak'
+        ], [
+            'Garlic', 'SaladBowl'
+        ], [
+            'CannedFood', 'Pear'
+        ], [
+            'BestMilk', 'BetterMilk', 'Chili', 'Chocolate', 'Milk', 'Sushi'
+        ], [
+            'Melon', 'Mushroom', 'Pizza', 'Steak'
+        ]
+    ]
+
     def test_instantiation(self):
-        self.fail()
+        food = Items()
+        food = [[item.__class__.__name__ for item in tier] for tier in food.items]
+        for i, tier in enumerate(food):
+            for j, obj in enumerate(tier):
+                self.assertTrue(obj == self.base_food[i][j], f"T{i+1}: {obj} should be {self.base_food[i][j]}")
