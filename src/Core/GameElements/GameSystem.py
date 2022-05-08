76afc21197b66e50e4e1dcc6fecdf022c55b8243
hunter <iamhunter32@gmail.com>
2022-05-08 05:32:01 -0400

    trying to flesh out game system

--- src/Core/GameElements/GameSystem.py
+++ src/Core/GameElements/GameSystem.py
@@ -1,8 +1,8 @@
 from .AbstractElements import Empty, Unarmed, Spawner, Team
 
 
-class GameSystem():
+class GameSystem:
     def __init__(self, mode):
         self._spawner = Spawner(mode)
-        self.team = Team([Empty() for _ in range(5)], [Unarmed() for _ in range(5)])
-    pass
+        self.team = Team()
+
--- src/Core/GameElements/__init__.py
+++ src/Core/GameElements/__init__.py
@@ -1,4 +1,6 @@
 from . import AbstractElements
 from . import Objects
+from .GameSystem import GameSystem
 from .Shop import Shop
-__all__ = ['AbstractElements', 'Objects', 'Shop']
+
+__all__ = ['AbstractElements', 'Objects', 'Shop', 'GameSystem']
