4786a233965712d4ff6e5337dfa07a312b3932cf
hunter <iamhunter32@gmail.com>
2022-05-08 05:33:16 -0400

    refactoring and dealing with changes to balance

--- src/Core/Overseer/MessageAgent.py
+++ src/Core/Overseer/MessageAgent.py
@@ -1,20 +1,29 @@
-from Core.Overseer.Handlers import *
+from .BaseAgent import BaseAgent
+from .Handlers.Items import Tier1, Tier2, Tier3, Tier4, Tier5, Tier6, Equipment
 
 
 animals = [
     "Nop",  # No action
+
     "Ant", "Beaver", "Beetle", "Bluebird", "Cricket", "Duck", "Fish", "Horse", "Ladybug", "Mosquito", "Otter",
     "Pig",  # Tier 1
-    "Bat", "Crab", "Dodo", "Dog", "Dromedary", "Elephant", "Flamingo", "Hedgehog", "Peacock", "Rat", "Shrimp",
+
+    "Bat", "Crab", "Dodo", "Dromedary", "Elephant", "Flamingo", "Hedgehog", "Peacock", "Rat", "Shrimp",
     "Spider", "Swan", "Tabby Cat",  # Tier 2
-    "Badger", "Blowfish", "Camel", "Caterpillar", "Giraffe", "Hatching Chick", "Kangaroo", "Owl", "Ox", "Puppy",
-    "Rabbit", "Sheep", "Snail", "Tropical Fish", "Turtle", "Whale",  # Tier 3
-    "Bison", "Buffalo", "Deer", "Dolphin", "Hippo", "Llama", "Lobster", "Monkey", "Penguin", "Poodle", "Rooster",
-    "Skunk", "Squirrel", "Worm",  # Tier 4
-    "Chicken", "Cow", "Crocodile", "Eagle", "Goat", "Microbe", "Parrot", "Rhino", "Scorpion", "Seal", "Shark",
+
+    "Badger", "Blowfish", "Camel", "Caterpillar", "Dog", "Giraffe", "Hatching Chick", "Kangaroo", "Owl", "Ox", "Puppy",
+    "Rabbit", "Sheep", "Snail", "Tropical Fish", "Turtle",  # Tier 3
+
+    "Bison", "Buffalo", "Deer", "Dolphin", "Hippo", "Llama", "Lobster", "Microbe", "Parrot", "Penguin",
+    "Rooster", "Skunk", "Squirrel", "Whale", "Worm",  # Tier 4
+
+    "Chicken", "Cow", "Crocodile", "Eagle", "Goat", "Monkey", "Poodle", "Rhino", "Scorpion", "Seal", "Shark",
     "Turkey",  # Tier 5
-    "Cat", "Dragon", "Fly", "Gorilla", "Leopard", "Mammoth", "Octopus", "Sauropod", "Snake", "Tiger",
+
+    "Boar", "Cat", "Dragon", "Fly", "Gorilla", "Leopard", "Mammoth", "Octopus", "Sauropod", "Snake", "Tiger",
     "Tyrannosaurus"  # Tier 6
+    
+    "Butterfly", "Bus", "DirtyRat", "FlyFriend", "Ram", "ZombieCricket"
 ]
 
 equipment = [
@@ -28,25 +37,40 @@ equipment = [
 
 
 # noinspection DuplicatedCode
-class MessageAgent(Equipment, Tier1, Tier2, Tier3, Tier4, Tier5, Tier6):
+class MessageAgent(BaseAgent):
     def __init__(self, mode):
         super(MessageAgent, self).__init__(mode)
 
         # event handling matrix
-        self.func = [self._nop,
-                     self._ant, self._beaver, self._beetle, self._bluebird, self._cricket, self._duck, self._fish,
-                     self._fish, self._horse, self._ladybug, self._mosquito, self._otter, self._pig,
-                     self._bat, self._crab, self._dodo, self._dog, self._dromedary, self._elephant, self._flamingo,
-                     self._hedgehog, self._peacock, self._rat, self._shrimp, self._spider, self._swan, self._tabby_cat,
-                     self._badger, self._blowfish, self._camel, self._caterpillar, self._giraffe, self._hatching_chick,
-                     self._kangaroo, self._owl, self._ox, self._puppy, self._rabbit, self._sheep, self._snail,
-                     self._tropical_fish, self._turtle, self._whale,
-                     self._bison, self._buffalo, self._deer, self._dolphin, self._hippo, self._llama, self._lobster,
-                     self._monkey, self._penguin, self._poodle, self._rooster, self._skunk, self._squirrel, self._worm,
-                     self._chicken, self._cow, self._crocodile, self._eagle, self._goat, self._microbe, self._parrot,
-                     self._rhino, self._scorpion, self._seal, self._shark, self._turkey,
-                     self._cat, self._dragon, self._fly, self._gorilla, self._leopard, self._mammoth, self._octopus,
-                     self._sauropod, self._snake, self._tiger, self._tyrannosaurus
+        t1 = Tier1()
+        t2 = Tier2()
+        t3 = Tier3()
+        t4 = Tier4()
+        t5 = Tier5()
+        t6 = Tier6()
+        eq = Equipment()
+
+        self.func = [t1.nop,
+                     t1.ant, t1.beaver, t1.beetle, t1.bluebird, t1.cricket, t1.duck, t1.fish,
+                     t1.horse, t1.ladybug, t1.mosquito, t1.otter, t1.pig,
+
+                     t2.bat, t2.crab, t2.dodo, t2.dromedary, t2.elephant, t2.flamingo,
+                     t2.hedgehog, t2.peacock, t2.rat, t2.shrimp, t2.spider, t2.swan, t2.tabby_cat,
+
+                     t3.badger, t3.blowfish, t3.camel, t3.caterpillar, t3.dog, t3.giraffe,
+                     t3.hatching_chick, t3.kangaroo, t3.owl, t3.ox, t3.puppy, t3.rabbit,
+                     t3.sheep, t3.snail, t3.tropical_fish, t3.turtle,
+
+                     t4.bison, t4.buffalo, t4.deer, t4.dolphin, t4.hippo, t4.llama, t4.lobster,
+                     t4.penguin, t4.poodle, t4.rooster, t4.skunk, t4.squirrel, t4.whale,
+                     t4.worm,
+
+                     t5.chicken, t5.cow, t5.crocodile, t5.eagle, t5.goat, t5.microbe, t5.monkey,
+                     t5.parrot, t5.rhino, t5.scorpion, t5.seal, t5.shark, t5.turkey,
+
+                     t6.boar, t6.cat, t6.dragon, t6.fly, t6.gorilla, t6.leopard, t6.mammoth,
+                     t6.octopus, t6.sauropod, t6.snake, t6.tiger, t6.tyrannosaurus
+
                      # self._zombie_cricket, self.dirty_rat, self.butterfly, self.honey, self
                      ]
 
@@ -65,10 +89,10 @@ class MessageAgent(Equipment, Tier1, Tier2, Tier3, Tier4, Tier5, Tier6):
 
     # make a new layer of handlers that deals with battle???
 
+    # message contains unit_id which sent it
     def handle_message(self, message):
         # trigger function that manipulates roster
-        self.lvl = self.team.level
-        self.func[message]()
+        self.func[message](self)
 
     # assume event which triggered event handler has already resolved!!!
     # e.g. if animal sold, then it is no longer in the roster and gold
