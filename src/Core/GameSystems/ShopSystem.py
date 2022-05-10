39fb2ed08fb47dbc83104abfd4e1c3a4ff01a43d
hunter <iamhunter32@gmail.com>
2022-05-10 04:36:47 -0400

    working on loop for shop system

--- src/Core/GameSystems/ShopSystem.py
+++ src/Core/GameSystems/ShopSystem.py
@@ -5,3 +5,18 @@ from Core.Overseer import MessageAgent
 class ShopSystem:
     def __init__(self, agent: MessageAgent):
         self.__agent = agent
+
+    def start_turn(self):
+        # load backup team into normal team
+
+        # clear shop
+
+        # events
+        # start turn
+        pass
+
+    def buy_item(self, item_pos: int, target_pos: int):
+        # differentiate between food/equipment and animals!
+        # presume it is a valid purchase
+
+        pass
