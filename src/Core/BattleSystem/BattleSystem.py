3ea533dafccdda69ff6aa9acfb1c7abba81023ee
hunter <iamhunter32@gmail.com>
2022-05-10 03:16:20 -0400

    battle loop logic overview, to be implemented

--- src/Core/BattleSystem/BattleSystem.py
+++ src/Core/BattleSystem/BattleSystem.py
@@ -1,6 +1,42 @@
+from ..GameElements.AbstractElements import Team, Animal
 from ..Overseer import *
 
 
 class BattleSystem:
-    def __init__(self, game_controller):
-        self.system = game_controller
+    def __init__(self, agent: MessageAgent):
+        self.__agent = agent
+
+    def start_battle(self, enemy: Team):
+        # start battle trigger
+
+        # loop:
+        # (team)
+        # before attack trigger,
+        # friend ahead attacks,
+        # enemy attacks,
+        # (enemy)
+        # before attack,
+        # friend ahead attacks,
+        # enemy attacks
+
+        # if team unit still above 0 hp:
+        # hurt
+
+        # if enemy unit still above 0 hp:
+        # hurt (enemy)
+
+        # if team unit not above 0 hp:
+        # on faint (team)
+        # friend faints
+        # friend ahead faints
+        # knock out (on enemy team)
+
+        # if enemy unit not above 0 hp:
+        # on faint (enemy)
+        # friend faints (enemy)
+        # friend ahead faints (enemy)
+        # knock out (on friendly team)
+        pass
+
+    def summon(self, unit: Animal, position: int):
+        pass
