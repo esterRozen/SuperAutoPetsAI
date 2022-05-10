1888a1edc5c9bc73b34dc1de910cb53a73403c6b
hunter <iamhunter32@gmail.com>
2022-05-10 01:40:32 -0400

    added some missing events

--- src/Core/Overseer/Handlers/Items/eventnames.py
+++ src/Core/Overseer/Handlers/Items/eventnames.py
@@ -1,10 +1,10 @@
 __all__ = [
     'BEFORE_ATTACK', 'BUY', 'BUY_FOOD', 'BUY_T1_PET', 'EAT_FOOD',
     'ENEMY_ATTACKS', 'END_TURN', 'FRIEND_AHEAD_ATTACKS',
-    'FRIEND_AHEAD_FAINTS', 'FRIEND_EATS_FOOD', 'FRIEND_FAINTS',
-    'FRIEND_SOLD', 'FRIEND_SUMMONED_BATTLE', 'FRIEND_SUMMONED_SHOP',
-    'HURT', 'KNOCK_OUT', 'ON_FAINT', 'ON_LEVEL', 'SELL',
-    'START_BATTLE', 'START_TURN'
+    'FRIEND_AHEAD_FAINTS', 'FRIEND_BOUGHT', 'FRIEND_EATS_FOOD',
+    'FRIEND_FAINTS', 'FRIEND_SOLD', 'FRIEND_SUMMONED_BATTLE',
+    'FRIEND_SUMMONED_SHOP', 'HURT', 'IS_SUMMONED', 'KNOCK_OUT',
+    'ON_FAINT', 'ON_LEVEL', 'SELL', 'START_BATTLE', 'START_TURN'
 ]
 
 BEFORE_ATTACK = "before attack"
@@ -16,12 +16,14 @@ ENEMY_ATTACKS = "enemy attacks"
 END_TURN = "end turn"
 FRIEND_AHEAD_ATTACKS = "friend ahead attacks"
 FRIEND_AHEAD_FAINTS = "friend ahead faints"
+FRIEND_BOUGHT = "friend bought"
 FRIEND_EATS_FOOD = "friend eats food"
 FRIEND_FAINTS = "friend faints"
 FRIEND_SOLD = "friend sold"
 FRIEND_SUMMONED_BATTLE = "friend summoned (battle)"
 FRIEND_SUMMONED_SHOP = "friend summoned (shop)"
 HURT = "hurt"
+IS_SUMMONED = "is summoned"
 KNOCK_OUT = "knock out"
 ON_FAINT = "on faint"
 ON_LEVEL = "on level"
