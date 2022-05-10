0402c2da8ecd98fd465a984c6ca98724c26d66d4
hunter <iamhunter32@gmail.com>
2022-05-10 03:31:17 -0400

    gave game system access method for sending agent messages

--- src/Core/GameElements/GameSystem.py
+++ src/Core/GameElements/GameSystem.py
@@ -1,8 +1,7 @@
 from .AbstractElements import Empty, Unarmed, Spawner, Team
+from ..Overseer import MessageAgent
 
 
 class GameSystem:
-    def __init__(self, mode):
-        self._spawner = Spawner(mode)
-        self.team = Team()
-
+    def __init__(self, agent: MessageAgent):
+        self.__agent = agent
