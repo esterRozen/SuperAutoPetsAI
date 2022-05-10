30db518fb5008615d30195c969d7580434591ae1
hunter <iamhunter32@gmail.com>
2022-05-09 23:34:25 -0400

    adding logic to handle determining which animal abilities to trigger.

--- src/Core/Overseer/Handlers/eventprocessor.py
+++ src/Core/Overseer/Handlers/eventprocessor.py
@@ -1,7 +1,7 @@
 from Core.Overseer import MessageAgent
 
 
-class TriggerProcessor:
+class EventProcessor:
     # acting should be the animal that is doing something
     # event raiser should be the animal whose triggers are going off.
 
@@ -11,61 +11,87 @@ class TriggerProcessor:
     # apply to unit that acted (was bought)
     @staticmethod
     def buy(agent: MessageAgent):
-        pass
+        operation = agent.team.animals[agent.event_raiser[1]].trigger("buy")
+        agent.trigger_ability(operation)
 
     # engine
     # apply to all units
     @staticmethod
     def buy_food(agent: MessageAgent):
-        pass
+        for animal in agent.sorted_team:
+            operation = animal.trigger("buy food")
+            agent.target = agent.team.animals.index(animal)
+            agent.trigger_ability(operation)
 
     # engine
     # apply to all units
     @staticmethod
     def buy_T1_pet(agent: MessageAgent):
-        pass
+        for animal in agent.sorted_team:
+            operation = animal.trigger("buy t1 pet")
+            agent.target = agent.team.animals.index(animal)
+            agent.trigger_ability(operation)
 
     # engine
     # apply to unit that acted (ate food)
     @staticmethod
     def eat_food(agent: MessageAgent):
-        pass
+        for animal in agent.sorted_team:
+            operation = animal.trigger("eat food")
+            agent.target = agent.team.animals.index(animal)
+            agent.trigger_ability(operation)
 
     # engine
     # apply to all units
     @staticmethod
     def start_turn(agent: MessageAgent):
-        pass
+        for animal in agent.sorted_team:
+            operation = animal.trigger("start turn")
+            agent.target = agent.team.animals.index(animal)
+            agent.trigger_ability(operation)
 
     # engine
     # apply to unit that raised event (got sold)
     @staticmethod
     def sell(agent: MessageAgent):
-        pass
+        operation = agent.team.animals[agent.event_raiser[1]].trigger("sell")
+        agent.trigger_ability(operation)
 
     # engine
     # apply to all units
     @staticmethod
     def end_turn(agent: MessageAgent):
-        pass
+        for animal in agent.sorted_team:
+            operation = animal.trigger("end turn")
+            agent.target = agent.team.animals.index(animal)
+            agent.trigger_ability(operation)
 
     # engine
     # apply to all units except event raiser
     @staticmethod
     def friend_bought(agent: MessageAgent):
-        pass
+        for animal in agent.sorted_without_raiser:
+            operation = animal.trigger("friend bought")
+            agent.target = agent.team.animals.index(animal)
+            agent.trigger_ability(operation)
 
     # engine
     # apply to all units except event raiser
     @staticmethod
     def friend_eats_food(agent: MessageAgent):
-        pass
+        for animal in agent.sorted_without_raiser:
+            operation = animal.trigger("friend eats food")
+            agent.target = agent.team.animals.index(animal)
+            agent.trigger_ability(operation)
 
     # engine
     # apply to all units
     @staticmethod
     def friend_sold(agent: MessageAgent):
-        pass
+        for animal in agent.sorted_team:
+            operation = animal.trigger("friend sold")
+            agent.target = agent.team.animals.index(animal)
+            agent.trigger_ability(operation)
 
     # engine
     # apply to units except event raiser
