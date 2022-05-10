bdc50d81565e060501c8067e30f1a0dfa3543031
hunter <iamhunter32@gmail.com>
2022-05-10 00:12:46 -0400

    making event names easier to maintain.

---
+++ src/Core/Overseer/Handlers/Items/eventnames.py
@@ -0,0 +1,30 @@
+__all__ = [
+    'BEFORE_ATTACK', 'BUY', 'BUY_FOOD', 'BUY_T1_PET', 'EAT_FOOD',
+    'ENEMY_ATTACKS', 'END_TURN', 'FRIEND_AHEAD_ATTACKS',
+    'FRIEND_AHEAD_FAINTS', 'FRIEND_EATS_FOOD', 'FRIEND_FAINTS',
+    'FRIEND_SOLD', 'FRIEND_SUMMONED_BATTLE', 'FRIEND_SUMMONED_SHOP',
+    'HURT', 'KNOCK_OUT', 'ON_FAINT', 'ON_LEVEL', 'SELL',
+    'START_BATTLE', 'START_TURN'
+]
+
+BEFORE_ATTACK = "before attack"
+BUY = "buy"
+BUY_FOOD = "buy food"
+BUY_T1_PET = "buy t1 pet"
+EAT_FOOD = "eat food"
+ENEMY_ATTACKS = "enemy attacks"
+END_TURN = "end turn"
+FRIEND_AHEAD_ATTACKS = "friend ahead attacks"
+FRIEND_AHEAD_FAINTS = "friend ahead faints"
+FRIEND_EATS_FOOD = "friend eats food"
+FRIEND_FAINTS = "friend faints"
+FRIEND_SOLD = "friend sold"
+FRIEND_SUMMONED_BATTLE = "friend summoned (battle)"
+FRIEND_SUMMONED_SHOP = "friend summoned (shop)"
+HURT = "hurt"
+KNOCK_OUT = "knock out"
+ON_FAINT = "on faint"
+ON_LEVEL = "on level"
+SELL = "sell"
+START_BATTLE = "start battle"
+START_TURN = "start turn"
