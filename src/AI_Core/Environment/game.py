0c3a0ec339ec76b3f985cfcaec236aed6ba72483
hunter <iamhunter32@gmail.com>
2022-05-10 04:02:00 -0400

    working on gym environment

--- src/AI_Core/Environment/game.py
+++ src/AI_Core/Environment/game.py
@@ -1,44 +1,52 @@
 from abc import ABC
+from typing import Optional, Union, Tuple
+
 import gym
-from gym import spaces
-import API
+from gym.spaces import MultiDiscrete, Discrete
+from gym.core import ObsType, ActType
+
+
+_num_elements = 150
+_num_equip = 30
+_max_gold = 25
+_max_turn = 20
 
 
 class SAP_Game(gym.Env):
     def __init__(self):
-        self.action_space = spaces.Tuple(
-            spaces.Discrete(7),
-            spaces.MultiDiscrete([5, 5, 7])
+        self.action_space = gym.spaces.Tuple(
+            Discrete(7),
+            MultiDiscrete([5, 5, 7])
         )
-        self.observation_space = spaces.Dict(
+        self.observation_space = gym.spaces.Dict(
             {
-                'TeamUnit1': spaces.MultiDiscrete([80, 50, 50, 8]),
-                'TeamUnit2': spaces.MultiDiscrete([80, 50, 50, 8]),
-                'TeamUnit3': spaces.MultiDiscrete([80, 50, 50, 8]),
-                'TeamUnit4': spaces.MultiDiscrete([80, 50, 50, 8]),
-                'TeamUnit5': spaces.MultiDiscrete([80, 50, 50, 8]),
-                'ShopSlot1': spaces.MultiDiscrete([80, 50, 50]),
-                'ShopSlot2': spaces.MultiDiscrete([80, 50, 50]),
-                'ShopSlot3': spaces.MultiDiscrete([80, 50, 50]),
-                'ShopSlot4': spaces.MultiDiscrete([80, 50, 50]),
-                'ShopSlot5': spaces.MultiDiscrete([80, 50, 50]),
-                'ShopSlot6': spaces.MultiDiscrete([80, 50, 50]),
-                'ShopSlot7': spaces.Discrete(15),
-                'ShopSlot8': spaces.Discrete(15),
-                'Gold': spaces.Discrete(15),
-                'Turn': spaces.Discrete(20)
+                'TeamUnit1': MultiDiscrete([_num_elements, 50, 50, _num_equip]),
+                'TeamUnit2': MultiDiscrete([_num_elements, 50, 50, _num_equip]),
+                'TeamUnit3': MultiDiscrete([_num_elements, 50, 50, _num_equip]),
+                'TeamUnit4': MultiDiscrete([_num_elements, 50, 50, _num_equip]),
+                'TeamUnit5': MultiDiscrete([_num_elements, 50, 50, _num_equip]),
+                'ShopSlot1': MultiDiscrete([_num_elements, 50, 50]),
+                'ShopSlot2': MultiDiscrete([_num_elements, 50, 50]),
+                'ShopSlot3': MultiDiscrete([_num_elements, 50, 50]),
+                'ShopSlot4': MultiDiscrete([_num_elements, 50, 50]),
+                'ShopSlot5': MultiDiscrete([_num_elements, 50, 50]),
+                'ShopSlot6': MultiDiscrete([_num_elements, 50, 50]),
+                'ShopSlot7': Discrete(_num_equip),
+                'ShopSlot8': Discrete(_num_equip),
+                'Gold': Discrete(_max_gold),
+                'Turn': Discrete(_max_turn)
             }
         )
 
-    def reset(self):
-        pass
-
-    def step(self, action):
-        #
-        # return state, reward, done, info
-        pass
-
     def load(self, state):
+        pass
+
+    def step(self, action: ActType)\
+            -> Tuple[ObsType, float, bool, dict]:
+        pass
+
+    def reset(self, *, seed: Optional[int] = None, return_info: bool = False, options: Optional[dict] = None)\
+            -> Union[ObsType, Tuple[ObsType, dict]]:
         pass
 
     def render(self, mode="human"):
