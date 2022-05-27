c2e64c14b21c658cc992d68b73d1dc7cc625f80b
hunter <ianrozen@buffalo.edu>
2022-05-27 00:28:02 -0400

    nearly ready to work on game system testing
    Took 49 seconds

--- tests/Core/game_systems/test_battle_system.py
+++ tests/Core/game_systems/test_battle_system.py
@@ -4,3 +4,9 @@ from unittest import TestCase
 class TestBattleSystem(TestCase):
     def test_instantiation(self):
         self.fail()
+
+    def test_start_battle(self):
+        self.fail()
+
+    def test_summon(self):
+        self.fail()
--- tests/Core/game_systems/test_shop_system.py
+++ tests/Core/game_systems/test_shop_system.py
@@ -4,3 +4,27 @@ from unittest import TestCase
 class TestShopSystem(TestCase):
     def test_instantiation(self):
         self.fail()
+
+    def test_start_turn(self):
+        self.fail()
+
+    def test_toggle_freeze(self):
+        self.fail()
+
+    def test_reroll(self):
+        self.fail()
+
+    def test_buy(self):
+        self.fail()
+
+    def test_sell(self):
+        self.fail()
+
+    def test_move(self):
+        self.fail()
+
+    def test_combine(self):
+        self.fail()
+
+    def test_end_turn(self):
+        self.fail()
