9c66301572bb484f2dbf11e8c67558441001fb65
hunter <ianrozen@buffalo.edu>
2022-05-26 20:19:16 -0400

    bug testing Team, added methods to make it indexable
    Took 1 hour 25 minutes

--- src/core/game_elements/abstract_elements/team.py
+++ src/core/game_elements/abstract_elements/team.py
@@ -1,121 +1,151 @@
 import random
-from typing import List
+from typing import List, Optional, Dict
 
 from . import Animal, Empty, Equipment, Unarmed
 
 
+# attacking animal is in position 0
 class Team:
     def __init__(self):
-        self.animals = [Empty() for _ in range(5)]
+        self.animals: List[Animal] = [Empty() for _ in range(5)]
         self.acting = 0
         self.__max_capacity = 5
         # random.seed(1)
 
-    @property
-    def size(self):
-        i = 0
-        for animal in self.animals:
-            if not isinstance(animal, Empty):
-                i += 1
-        return i
+    def __delitem__(self, key):
+        self.animals[key] = Empty()
+
+    def __getitem__(self, item: int) -> Animal:
+        return self.animals[item]
+
+    def __setitem__(self, idx: int, value: Animal):
+        self.animals[idx] = value
 
     @property
-    def has_lvl3(self):
+    def has_lvl3(self) -> bool:
         for animal in self.animals:
             if animal.level == 3:
                 return True
         return False
 
     @property
-    def level_of_actor(self):
+    def has_summon_space(self) -> bool:
+        return self.size < self.__max_capacity
+
+    @property
+    def leftmost_unit(self) -> Optional[Animal]:
+        if self.size == 0:
+            return None
+
+        i = len(self.animals)-1
+        while isinstance(self.animals[i], Empty):
+            i -= 1
+        return self.animals[i]
+
+    @property
+    def level_of_actor(self) -> int:
         # level of currently acting team member
         return self.animals[self.acting].level
 
     @property
-    def has_summon_space(self):
-        return self.size < self.__max_capacity
-
-    def lowest_health(self):
-        return min(self.animals, key=lambda animal: animal.battle_hp)
-
-    def ret_diff_tiers(self):
-        pass
-
-    def faint(self, target):
-        self.animals[target] = Empty()
-
-    def push_forward(self):
-        # TODO push_forward
-        return self
-
-    def summon(self, animal, position):
-        if self.size == 5:
-            return self
-        # insert unit to that position, if there would be too many units, insert and delete excess.
-        self.animals.insert(animal, position)
-
-    def leftmost_unit(self):
+    def rightmost_unit(self) -> Optional[Animal]:
+        if self.size == 0:
+            return None
         i = 0
         while isinstance(self.animals[i], Empty):
             # TODO will probably have to make it check for battle hp?
             i += 1
         return self.animals[i]
 
-    def rightmost_unit(self):
-        if self.size == 0:
+    @property
+    def second_unit(self) -> Optional[Animal]:
+        if self.size <= 1:
             return None
-        i = len(self.animals)-1
-        while isinstance(self.animals[i], Empty):
-            i -= 1
-        return self.animals[i]
+        j = 0
+        i = 0
+        while j != 2 and i < self.__max_capacity:
+            if not isinstance(self.animals[i], Empty):
+                j += 1
+            i += 1
+        return self.animals[i-1]
 
-    def friend_ahead(self):
-        for j in range(self.acting, len(self.animals)):
-            if isinstance(self.animals[j], Empty):
+    @property
+    def size(self) -> int:
+        i = 0
+        for animal in self.animals:
+            if not isinstance(animal, Empty):
+                i += 1
+        return i
+
+    def faint(self, target):
+        self.animals[target] = Empty()
+
+    def friends(self) -> Optional[List[Animal]]:
+        a = list(range(0, 5))
+        a.remove(self.acting)
+        for i in a:
+            if isinstance(self.animals[i], Empty):
+                a.remove(i)
+        if not a:
+            return None
+        return [self.animals[i] for i in a]
+
+    def friend_ahead(self) -> Optional[Animal]:
+        for j in range(self.acting-1, -1, -1):
+            if not isinstance(self.animals[j], Empty):
                 return self.animals[j]
         return None
 
-    def other_lvl2_or_3(self):
-        a: List[int] = list(range(0, 5))
+    def friends_ahead(self, n) -> Optional[List[Animal]]:
+        ret = []
+        i = 0
+        for j in range(self.acting-1, -1, -1):
+            if not isinstance(self.animals[j], Empty):
+                if i < n:
+                    ret += [self.animals[j]]
+                    i += 1
+        if not ret:
+            return None
+        return ret
+
+    def friend_behind(self) -> Optional[Animal]:
+        for j in range(self.acting+1, len(self.animals)):
+            if not isinstance(self.animals[j], Empty):
+                return self.animals[j]
+        return None
+
+    def friends_behind(self, n) -> Optional[List[Animal]]:
+        ret = []
+        i = 0
+        for j in range(self.acting+1, len(self.animals)):
+            if not isinstance(self.animals[j], Empty):
+                if i < n:
+                    ret += [self.animals[j]]
+                    i += 1
+        if not ret:
+            return None
+        return ret
+
+    def lowest_health_unit(self):
+        return min(self.animals, key=lambda animal: animal.battle_hp)
+
+    def other_lvl2_or_3(self) -> Optional[List[Animal]]:
+        a = list(range(0, 5))
         a.remove(self.acting)
         for i in a:
             if self.animals[i].level == 1:
                 a.remove(i)
+        if not a:
+            return None
         return [self.animals[i] for i in a]
 
-    def friends_ahead(self, n):
-        ret = []
-        i = 0
-        for j in range(self.acting, len(self.animals)):
-            if isinstance(self.animals[j], Empty):
-                if i < n:
-                    ret += [self.animals[j]]
-                    i += 1
-        if not ret:
-            return None
-        return ret
-
     # TODO fix these to have None-case in event of no valid outputs
-    def friend_behind(self):
-        for j in range(self.acting-1, -1, -1):
-            if isinstance(self.animals[j], Empty):
-                return self.animals[j]
-        return None
+    def push_forward(self):
+        # TODO push_forward
+        return self
 
-    def friends_behind(self, n):
-        ret = []
-        i = 0
-        for j in range(self.acting-1, -1, -1):
-            if isinstance(self.animals[j], Empty):
-                if i < n:
-                    ret += [self.animals[j]]
-                    i += 1
-        if not ret:
-            return None
-        return ret
-
-    def random_friend(self):
-        a: List[int] = list(range(0, 5))
+    def random_friend(self) -> Optional[Animal]:
+        a = list(range(0, 5))
         a.remove(self.acting)
         for i in a:
             if isinstance(self.animals[i], Empty):
@@ -124,55 +154,70 @@ class Team:
             return None
         return self.animals[random.choice(a)]
 
-    def friends(self):
-        a: List[int] = list(range(0, 5))
-        a.remove(self.acting)
-        for i in a:
-            if isinstance(self.animals[i], Empty):
-                a.remove(i)
-        return [self.animals[i] for i in a]
-
-    def random_friends(self, n):
+    def random_friends(self, n) -> Optional[List[Animal]]:
         a = list(range(0, 5))
         a.remove(self.acting)
         for i in a:
             if isinstance(self.animals[i], Empty):
                 a.remove(i)
         if not a:
-            return []
+            return None
         if len(a) < n:
             return [self.animals[i] for i in a]
         else:
             return random.sample([self.animals[i] for i in a], n)
 
-    def random_unit(self):
+    def random_unit(self) -> Optional[Animal]:
+        if self.size == 0:
+            return None
+
         a = list(range(0, 5))
         for i in a:
             if isinstance(self.animals[i], Empty):
                 a.remove(i)
 
-        return self.animals[random.choice(list(range(0, 5)))]
+        return self.animals[random.choice(a)]
 
-    def random_units(self, n):
+    def random_units(self, n) -> Optional[List[Animal]]:
+        if self.size == 0:
+            return None
+
         a = list(range(0, 5))
         for i in a:
             if isinstance(self.animals[i], Empty):
                 a.remove(i)
         if not a:
             return []
-        if len(a) < n:
+        if len(a) <= n:
             return [self.animals[i] for i in a]
         else:
             return random.sample([self.animals[i] for i in a], n)
 
-    def random_units_idx(self, n):
+    def random_units_idx(self, n) -> Optional[List[int]]:
         a = list(range(0, 5))
         for i in a:
             if isinstance(self.animals[i], Empty):
                 a.remove(i)
         if not a:
-            return []
-        if len(a) < n:
-            return [self.animals[i] for i in a]
+            return None
+        if len(a) <= n:
+            return a
         else:
             return random.sample(a, n)
+
+    def ret_diff_tiers(self) -> Optional[List[Animal]]:
+        animals: Dict = {}
+        for animal in self.animals:
+            animals[animal.tier] = animal
+
+        animals: List[Animal] = list(animals.values())
+        if not animals:
+            return None
+        return animals
+
+    def summon(self, animal, position):
+        # TODO fix this
+        if self.size == 5:
+            return self
+        # insert unit to that position, if there would be too many units, insert and delete excess.
+        self.animals.insert(animal, position)
--- tests/Core/game_elements/abstract_elements/test_team.py
+++ tests/Core/game_elements/abstract_elements/test_team.py
@@ -6,9 +6,91 @@ class TestTeam(TestCase):
     spawner = Spawner("paid_1")
     team = Team()
 
-    def clean_start(self, mode: str):
+    def clean_start(self, mode: str = "base"):
         self.spawner = Spawner(mode)
         self.team = Team()
+
+    def test_has_lvl3(self):
+        spawner = Spawner("paid_1")
+        team = Team()
+        for i in range(5):
+            team.animals[i] = spawner.spawn(1)
+            self.assertFalse(team.has_lvl3, "team should not have any level 3 units")
+
+        team.animals[3].increase_xp(5)
+        self.assertTrue(team.has_lvl3, "team should have a level 3")
+
+    def test_leftmost_unit(self):
+        self.clean_start()
+        self.team[1] = self.spawner.spawn(3)
+        self.team[3] = self.spawner.spawn(6)
+        self.team[0] = self.spawner.spawn(5)
+        self.team[2] = self.spawner.spawn(4)
+        self.assertTrue(self.team.leftmost_unit == self.team[3])
+
+        self.clean_start()
+        self.team[0] = self.spawner.spawn(4)
+        self.team[4] = self.spawner.spawn(3)
+        self.assertTrue(self.team.leftmost_unit == self.team[4])
+
+        self.clean_start()
+        self.team[3] = self.spawner.spawn(5)
+        self.assertTrue(self.team.leftmost_unit == self.team[3])
+
+        self.clean_start()
+        self.assertTrue(self.team.leftmost_unit is None, "should be none due as there are no units present")
+
+    def test_level_of_actor(self):
+        self.clean_start()
+        for i in range(5):
+            self.team.animals[i] = self.spawner.spawn(3)
+            self.team.animals[i].increase_xp(i+1)
+            self.assertTrue(self.team.level_of_actor == 1, "actor shouldn't change")
+
+        levels = [1, 2, 2, 2, 3]
+        for i in range(5):
+            self.team.acting = i
+            self.assertTrue(self.team.level_of_actor == levels[i], f"actor should be lvl {1+(i+2)//3}")
+
+    def test_rightmost_unit(self):
+        self.clean_start()
+        self.team[1] = self.spawner.spawn(3)
+        self.team[3] = self.spawner.spawn(6)
+        self.team[4] = self.spawner.spawn(5)
+        self.team[2] = self.spawner.spawn(4)
+        self.assertTrue(self.team.rightmost_unit == self.team[1])
+
+        self.clean_start()
+        self.team[0] = self.spawner.spawn(4)
+        self.team[4] = self.spawner.spawn(3)
+        self.assertTrue(self.team.rightmost_unit == self.team[0])
+
+        self.clean_start()
+        self.team[3] = self.spawner.spawn(5)
+        self.assertTrue(self.team.rightmost_unit == self.team[3])
+
+        self.clean_start()
+        self.assertTrue(self.team.rightmost_unit is None, "should be none due as there are no units present")
+
+    def test_second_unit(self):
+        self.clean_start()
+        self.team[1] = self.spawner.spawn(3)
+        self.team[3] = self.spawner.spawn(6)
+        self.team[4] = self.spawner.spawn(5)
+        self.team[2] = self.spawner.spawn(4)
+        self.assertTrue(self.team.second_unit == self.team[2])
+
+        self.clean_start()
+        self.team[0] = self.spawner.spawn(4)
+        self.team[4] = self.spawner.spawn(3)
+        self.assertTrue(self.team.second_unit == self.team[4])
+
+        self.clean_start()
+        self.team[3] = self.spawner.spawn(5)
+        self.assertTrue(self.team.second_unit is None, "should be none as there is only 1 unit")
+
+        self.clean_start()
+        self.assertTrue(self.team.rightmost_unit is None, "should be none due as there are no units present")
 
     def test_size(self):
         spawner = Spawner("paid_1")
@@ -26,38 +108,18 @@ class TestTeam(TestCase):
         team.push_forward()
         self.assertTrue(team.size == 3, "team should still have 3 units on it")
 
-    def test_has_lvl3(self):
-        spawner = Spawner("paid_1")
-        team = Team()
-        for i in range(5):
-            team.animals[i] = spawner.spawn(1)
-            self.assertFalse(team.has_lvl3, "team should not have any level 3 units")
-
-        team.animals[3].increase_xp(5)
-        self.assertTrue(team.has_lvl3, "team should have a level 3")
-
-    def test_level_of_actor(self):
-        self.clean_start("base")
-        for i in range(5):
-            self.team.animals[i] = self.spawner.spawn(3)
-            self.team.animals[i].increase_xp(i+1)
-            self.assertTrue(self.team.level_of_actor == 1, "actor shouldn't change")
-
-        levels = [1, 2, 2, 2, 3]
-        for i in range(5):
-            self.team.acting = i
-            self.assertTrue(self.team.level_of_actor == levels[i], f"actor should be lvl {1+(i+2)//3}")
-
     def test_lowest_health(self):
         self.clean_start("paid_1")
         for i in range(5):
             self.team.animals[i] = self.spawner.spawn(4)
             if i != 3:
                 self.team.animals[i].permanent_buff(15, 15)
-        self.assertTrue(self.team.animals[3] == self.team.lowest_health())
+        self.assertTrue(self.team.animals[3] == self.team.lowest_health_unit())
+
+    #####################################
 
     def test_ret_diff_tiers(self):
-        self.fail("function not implemented")
+        self.fail()
 
     def test_faint(self):
         self.fail()
@@ -68,14 +130,28 @@ class TestTeam(TestCase):
     def test_summon(self):
         self.fail()
 
-    def test_leftmost_unit(self):
-        self.fail()
-
-    def test_rightmost_unit(self):
-        self.fail()
-
     def test_friend_ahead(self):
-        self.fail()
+        self.clean_start()
+        self.team[0] = self.spawner.spawn(3)
+        self.team[2] = self.spawner.spawn(4)
+
+        self.team.acting = 2
+        self.assertTrue(self.team.friend_ahead() == self.team[0], "only unit ahead of position 2 is in position 0")
+
+        self.clean_start()
+        self.team[1] = self.spawner.spawn(5)
+        self.team[2] = self.spawner.spawn(3)
+        self.team[3] = self.spawner.spawn(4)
+        self.team[4] = self.spawner.spawn(5)
+
+        self.team.acting = 3
+        self.assertTrue(self.team.friend_ahead() == self.team[2])
+
+        self.clean_start()
+        self.team[0] = self.spawner.spawn(5)
+
+        self.team.acting = 0
+        self.assertTrue(self.team.friend_ahead() is None)
 
     def test_other_lvl2_or_3(self):
         self.fail()
@@ -84,7 +160,27 @@ class TestTeam(TestCase):
         self.fail()
 
     def test_friend_behind(self):
-        self.fail()
+        self.clean_start()
+        self.team[0] = self.spawner.spawn(3)
+        self.team[2] = self.spawner.spawn(4)
+
+        self.team.acting = 0
+        self.assertTrue(self.team.friend_behind() == self.team[2], "only unit ahead of position 2 is in position 0")
+
+        self.clean_start()
+        self.team[1] = self.spawner.spawn(5)
+        self.team[2] = self.spawner.spawn(3)
+        self.team[3] = self.spawner.spawn(4)
+        self.team[4] = self.spawner.spawn(5)
+
+        self.team.acting = 3
+        self.assertTrue(self.team.friend_behind() == self.team[4])
+
+        self.clean_start()
+        self.team[0] = self.spawner.spawn(5)
+
+        self.team.acting = 0
+        self.assertTrue(self.team.friend_behind() is None)
 
     def test_friends_behind(self):
         self.fail()
