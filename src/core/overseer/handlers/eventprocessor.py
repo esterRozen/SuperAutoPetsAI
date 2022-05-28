4ec0b0dae2de93d1a85f6a18df779f823bf1e86b
hunter <ianrozen@buffalo.edu>
2022-05-28 19:58:17 -0400

    refactor out the common functions.

--- src/core/overseer/handlers/eventprocessor.py
+++ src/core/overseer/handlers/eventprocessor.py
@@ -1,38 +1,91 @@
 from typing import TYPE_CHECKING, Tuple
 
 from ...eventnames import *
+
 if TYPE_CHECKING:
     from .. import MessageAgent
 
 
-def event_raiser_is_team(event_raiser: Tuple[str, int]) -> bool:
-    return event_raiser[0] == "team"
+def actor_is_team(actor: Tuple[str, int]) -> bool:
+    if actor[0] == "team":
+        return True
+    elif actor[0] == "enemy":
+        return False
+    else:
+        raise ValueError(f"Invalid team type {actor[0]}")
 
 
-def event_raiser_is_enemy(event_raiser: Tuple[str, int]) -> bool:
-    return event_raiser[0] == "enemy"
+def get_roster(agent: 'MessageAgent', actor: Tuple[str, int]):
+    if actor[0] == "team":
+        roster = agent.team
+    elif actor[0] == "enemy":
+        roster = agent.enemy
+    else:
+        raise ValueError(f"Invalid team type {actor[0]}")
+    return roster
 
 
-def for_sorted_team_trigger_non_target(agent: 'MessageAgent', event: str):
-    for animal in agent.sorted_team:
-        actor = agent.team.animals.index(animal)
-        agent.team.acting = actor
-        agent.event_raiser = ("team", actor)
-
-        operation = animal.trigger(event)
-        agent.trigger_ability(operation)
-
-
-def for_sorted_without_raiser_trigger_non_target(agent: 'MessageAgent', event: str):
-    animals = agent.sorted_without_raiser
+def for_sorted_trigger(agent: 'MessageAgent', event: str, team: str = "team"):
+    roster = get_roster(agent, (team, 0))
+    animals = agent.sorted_team(team)
 
     for animal in animals:
-        actor = agent.team.animals.index(animal)
-        agent.team.acting = actor
-        agent.event_raiser = ("team", actor)
+        actor = roster.animals.index(animal)
+        roster.acting = actor
+        agent.event_raiser = (team, actor)
 
         operation = animal.trigger(event)
         agent.trigger_ability(operation)
+
+
+def for_sorted_both_trigger(agent: 'MessageAgent', event: str):
+    animals = agent.sorted_team("both")
+
+    for animal in animals:
+        if animal in agent.team.animals:
+            agent.team.acting = agent.team.animals.index(animal)
+            agent.event_raiser = ("team", agent.team.acting)
+        elif animal in agent.enemy.animals:
+            agent.enemy.acting = agent.enemy.animals.index(animal)
+            agent.event_raiser = ("enemy", agent.enemy.acting)
+        operation = animal.trigger(event)
+        agent.trigger_ability(operation)
+
+
+def for_sorted_without_actor_trigger_(agent: 'MessageAgent', actor: Tuple[str, int], event: str):
+    roster = get_roster(agent, actor)
+    animals = agent.sorted_without_(actor)
+    team = actor[0]
+
+    for animal in animals:
+        actor = roster.animals.index(animal)
+        roster.acting = actor
+        agent.event_raiser = (team, actor)
+
+        operation = animal.trigger(event)
+        agent.trigger_ability(operation)
+
+
+def for_sorted_behind_(agent: 'MessageAgent', actor: Tuple[str, int], event: str):
+    roster = get_roster(agent, actor)
+    animals = agent.sorted_units_behind_(actor)
+    team = actor[0]
+
+    for animal in animals:
+        actor = roster.animals.index(animal)
+        roster.acting = actor
+        agent.event_raiser = (team, actor)
+
+        operation = animal.trigger(event)
+        agent.trigger_ability(operation)
+
+
+def trigger_actor_ability(agent: 'MessageAgent', actor: Tuple[str, int], event: str):
+    team = get_roster(agent, actor)
+    team.acting = actor[1]
+    agent.event_raiser = actor
+    operation = team.animals[actor[1]].trigger(event)
+    agent.trigger_ability(operation)
 
 
 class EventProcessor:
@@ -43,235 +96,144 @@ class EventProcessor:
 
     ################################################################
 
-    # TODO make sure each sets the actor and target properly
-    #  presume the event_raiser is correct, but it may change after ability trigger!!
-
     # shop
     # apply to unit that acted (was bought)
     @staticmethod
-    def buy(agent: 'MessageAgent'):
-        actor = agent.event_raiser[1]
-        agent.team.acting = actor
-        operation = agent.team.animals[actor].trigger(BUY)
-        agent.trigger_ability(operation)
+    def buy(agent: 'MessageAgent', actor: Tuple[str, int]):
+        trigger_actor_ability(agent, actor, BUY)
 
     # shop
     # apply to all units
     @staticmethod
     def buy_food(agent: 'MessageAgent'):
-        for_sorted_team_trigger_non_target(agent, BUY_FOOD)
+        for_sorted_trigger(agent, BUY_FOOD)
 
     # shop
     # apply to all units
     @staticmethod
     def buy_t1_pet(agent: 'MessageAgent'):
-        for_sorted_team_trigger_non_target(agent, BUY_T1_PET)
+        for_sorted_trigger(agent, BUY_T1_PET)
 
     # engine
     # apply to unit that acted (ate food)
     @staticmethod
-    def eat_food(agent: 'MessageAgent'):
-        actor = agent.event_raiser[1]
-        agent.team.acting = actor
-
-        operation = agent.team.animals[actor].trigger(EAT_FOOD)
-        agent.trigger_ability(operation)
+    def eat_food(agent: 'MessageAgent', actor: Tuple[str, int]):
+        trigger_actor_ability(agent, actor, EAT_FOOD)
 
     # shop
     # apply to all units
     @staticmethod
     def end_turn(agent: 'MessageAgent'):
-        for_sorted_team_trigger_non_target(agent, END_TURN)
+        for_sorted_trigger(agent, END_TURN)
 
     # shop
     # apply to all units except event raiser
     @staticmethod
-    def friend_bought(agent: 'MessageAgent'):
-        for_sorted_without_raiser_trigger_non_target(agent, FRIEND_BOUGHT)
+    def friend_bought(agent: 'MessageAgent', actor: Tuple[str, int]):
+        for_sorted_without_actor_trigger_(agent, actor, FRIEND_BOUGHT)
 
     # shop
     # apply to all units except event raiser
     @staticmethod
-    def friend_eats_food(agent: 'MessageAgent'):
-        for_sorted_without_raiser_trigger_non_target(agent, FRIEND_EATS_FOOD)
+    def friend_eats_food(agent: 'MessageAgent', actor: Tuple[str, int]):
+        for_sorted_without_actor_trigger_(agent, actor, FRIEND_EATS_FOOD)
 
     # shop
     # apply to all units except event raiser
     @staticmethod
-    def friend_sold(agent: 'MessageAgent'):
-        for_sorted_without_raiser_trigger_non_target(agent, FRIEND_SOLD)
+    def friend_sold(agent: 'MessageAgent', actor: Tuple[str, int]):
+        for_sorted_without_actor_trigger_(agent, actor, FRIEND_SOLD)
 
     # shop
     # apply to units except event raiser
     @staticmethod
-    def friend_summoned_shop(agent: 'MessageAgent'):
-        for_sorted_without_raiser_trigger_non_target(agent, FRIEND_SUMMONED_SHOP)
+    def friend_summoned_shop(agent: 'MessageAgent', actor: Tuple[str, int]):
+        for_sorted_without_actor_trigger_(agent, actor, FRIEND_SUMMONED_SHOP)
 
     # shop
     # apply to unit that raised event (got sold)
     @staticmethod
-    def sell(agent: 'MessageAgent'):
-        actor = agent.event_raiser[1]
-        agent.team.acting = actor
-
-        operation = agent.team.animals[actor].trigger(SELL)
-        agent.trigger_ability(operation)
+    def sell(agent: 'MessageAgent', actor: Tuple[str, int]):
+        trigger_actor_ability(agent, actor, SELL)
 
     # shop
     # apply to all units
     @staticmethod
     def start_turn(agent: 'MessageAgent'):
-        for_sorted_team_trigger_non_target(agent, START_TURN)
+        for_sorted_trigger(agent, START_TURN)
 
     ################################################################
 
     # shop and battle system
     # apply to friendly units right of event raiser
     @staticmethod
-    def friend_ahead_faints(agent: 'MessageAgent'):
-        if event_raiser_is_team(agent.event_raiser):
-            for animal in agent.sorted_units_behind_raiser:
-                operation = animal.trigger(FRIEND_AHEAD_FAINTS)
-                agent.target = ("team", agent.team.animals.index(animal))
-                agent.trigger_ability(operation)
-        elif event_raiser_is_enemy(agent.event_raiser):
-            for animal in agent.sorted_units_behind_raiser:
-                operation = animal.trigger(FRIEND_AHEAD_FAINTS)
-                agent.target = ("enemy", agent.enemy.animals.index(animal))
-                agent.trigger_ability(operation)
+    def friend_ahead_faints(agent: 'MessageAgent', actor: Tuple[str, int]):
+        for_sorted_behind_(agent, actor, FRIEND_AHEAD_FAINTS)
 
     # shop and battle system
     # apply to all friendly units
     @staticmethod
-    def friend_faints(agent: 'MessageAgent'):
-        if event_raiser_is_team(agent.event_raiser):
-            for animal in agent.sorted_team:
-                operation = animal.trigger(FRIEND_FAINTS)
-                agent.target = ("team", agent.team.animals.index(animal))
-                agent.trigger_ability(operation)
-        elif event_raiser_is_enemy(agent.event_raiser):
-            for animal in agent.sorted_team:
-                operation = animal.trigger(FRIEND_FAINTS)
-                agent.target = ("enemy", agent.enemy.animals.index(animal))
-                agent.trigger_ability(operation)
+    def friend_faints(agent: 'MessageAgent', actor: Tuple[str, int]):
+        for_sorted_trigger(agent, FRIEND_FAINTS, actor[0])
 
     # shop and battle system
     # apply to unit that raised event (got hurt)
     @staticmethod
-    def hurt(agent: 'MessageAgent'):
-        if event_raiser_is_team(agent.event_raiser):
-            operation = agent.team.animals[agent.event_raiser[1]].trigger(HURT)
-            agent.trigger_ability(operation)
-        elif event_raiser_is_enemy(agent.event_raiser):
-            operation = agent.enemy.animals[agent.event_raiser[1]].trigger(HURT)
-            agent.trigger_ability(operation)
+    def hurt(agent: 'MessageAgent', actor: Tuple[str, int]):
+        trigger_actor_ability(agent, actor, HURT)
 
     # both
     # apply to unit that raised event (was summoned)
     @staticmethod
-    def is_summoned(agent: 'MessageAgent'):
-        if event_raiser_is_team(agent.event_raiser):
-            operation = agent.team.animals[agent.event_raiser[1]].trigger(IS_SUMMONED)
-            agent.trigger_ability(operation)
-        elif event_raiser_is_enemy(agent.event_raiser):
-            operation = agent.enemy.animals[agent.event_raiser[1]].trigger(IS_SUMMONED)
-            agent.trigger_ability(operation)
+    def is_summoned(agent: 'MessageAgent', actor: Tuple[str, int]):
+        trigger_actor_ability(agent, actor, IS_SUMMONED)
 
     # shop and battle system
     # apply to unit that raised event (got knocked out)
     @staticmethod
-    def on_faint(agent: 'MessageAgent'):
-        if event_raiser_is_team(agent.event_raiser):
-            operation = agent.team.animals[agent.event_raiser[1]].trigger(ON_FAINT)
-            agent.trigger_ability(operation)
-        elif event_raiser_is_enemy(agent.event_raiser):
-            operation = agent.enemy.animals[agent.event_raiser[1]].trigger(ON_FAINT)
-            agent.trigger_ability(operation)
+    def on_faint(agent: 'MessageAgent', actor: Tuple[str, int]):
+        trigger_actor_ability(agent, actor, ON_FAINT)
 
     # shop and battle system
     # apply to unit that raised event (went up a level)
     @staticmethod
-    def on_level(agent: 'MessageAgent'):
-        if event_raiser_is_team(agent.event_raiser):
-            operation = agent.team.animals[agent.event_raiser[1]].trigger(ON_LEVEL)
-            agent.trigger_ability(operation)
-        elif event_raiser_is_enemy(agent.event_raiser):
-            operation = agent.enemy.animals[agent.event_raiser[1]].trigger(ON_FAINT)
-            agent.trigger_ability(operation)
+    def on_level(agent: 'MessageAgent', actor: Tuple[str, int]):
+        trigger_actor_ability(agent, actor, ON_LEVEL)
 
     ################################################################
 
     # triggered by battle system (which feeds message to message agent)
     # apply to unit that raised event (is about to attack)
     @staticmethod
-    def before_attack(agent: 'MessageAgent'):
-        if event_raiser_is_team(agent.event_raiser):
-            operation = agent.team.animals[agent.event_raiser[1]].trigger(BEFORE_ATTACK)
-            agent.trigger_ability(operation)
-        elif event_raiser_is_enemy(agent.event_raiser):
-            operation = agent.enemy.animals[agent.event_raiser[1]].trigger(BEFORE_ATTACK)
-            agent.trigger_ability(operation)
+    def before_attack(agent: 'MessageAgent', actor: Tuple[str, int]):
+        trigger_actor_ability(agent, actor, BEFORE_ATTACK)
 
     # battle system
     # apply to all friendly units
     @staticmethod
-    def enemy_attacks(agent: 'MessageAgent'):
-        if event_raiser_is_team(agent.event_raiser):
-            for animal in agent.sorted_team:
-                operation = animal.trigger(ENEMY_ATTACKS)
-                agent.target = ("team", agent.team.animals.index(animal))
-                agent.trigger_ability(operation)
-        elif event_raiser_is_enemy(agent.event_raiser):
-            for animal in agent.sorted_team:
-                operation = animal.trigger(ENEMY_ATTACKS)
-                agent.target = ("enemy", agent.enemy.animals.index(animal))
-                agent.trigger_ability(operation)
+    def enemy_attacks(agent: 'MessageAgent', actor: Tuple[str, int]):
+        for_sorted_trigger(agent, ENEMY_ATTACKS, actor[0])
 
     # battle system
     # apply to all friendly units left of event raiser
     @staticmethod
-    def friend_ahead_attacks(agent: 'MessageAgent'):
-        if event_raiser_is_team(agent.event_raiser):
-            for animal in agent.sorted_units_behind_raiser:
-                operation = animal.trigger(FRIEND_AHEAD_ATTACKS)
-                agent.target = ("team", animal.team.animals.index(animal))
-                agent.trigger_ability(operation)
-        elif event_raiser_is_enemy(agent.event_raiser):
-            for animal in agent.sorted_units_behind_raiser:
-                operation = animal.trigger(FRIEND_AHEAD_ATTACKS)
-                agent.target = ("enemy", agent.enemy.animals.index(animal))
-                agent.trigger_ability(operation)
+    def friend_ahead_attacks(agent: 'MessageAgent', actor: Tuple[str, int]):
+        for_sorted_behind_(agent, actor, FRIEND_AHEAD_ATTACKS)
 
     # battle system
     # apply to all friendly units except event raiser
     @staticmethod
-    def friend_summoned_battle(agent: 'MessageAgent'):
-        if event_raiser_is_team(agent.event_raiser):
-            for animal in agent.sorted_without_raiser:
-                operation = animal.trigger(FRIEND_SUMMONED_BATTLE)
-                agent.target = ("team", agent.team.animals.index(animal))
-                agent.trigger_ability(operation)
-        elif event_raiser_is_enemy(agent.event_raiser):
-            for animal in agent.sorted_without_raiser:
-                operation = animal.trigger(FRIEND_SUMMONED_BATTLE)
-                agent.target = ("enemy", agent.enemy.animals.index(animal))
-                agent.trigger_ability(operation)
+    def friend_summoned_battle(agent: 'MessageAgent', actor: Tuple[str, int]):
+        for_sorted_without_actor_trigger_(agent, actor, FRIEND_SUMMONED_BATTLE)
 
     # battle system
     # apply to unit acting (which knocked unit out)
     @staticmethod
-    def knock_out(agent: 'MessageAgent'):
-        if event_raiser_is_team(agent.event_raiser):
-            operation = agent.team.animals[agent.event_raiser[1]].trigger(KNOCK_OUT)
-            agent.trigger_ability(operation)
-        elif event_raiser_is_enemy(agent.event_raiser):
-            operation = agent.team.animals[agent.event_raiser[1]].trigger(KNOCK_OUT)
-            agent.trigger_ability(operation)
+    def knock_out(agent: 'MessageAgent', actor: Tuple[str, int]):
+        trigger_actor_ability(agent, actor, KNOCK_OUT)
 
     # battle system
     # apply to all units on both teams
     @staticmethod
     def start_battle(agent: 'MessageAgent'):
-        for animal in agent.sorted_team(team="both"):
-            operation = animal.trigger(START_BATTLE)
-            agent.trigger_ability(operation)
+        for_sorted_both_trigger(agent, START_BATTLE)
