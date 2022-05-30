ffa53343c48c32f1a8c04c56854aefb5a2840cac
hunter <ianrozen@buffalo.edu>
2022-05-29 22:57:35 -0400

    new memento based state, also has equality checks

--- src/core/overseer/state.py
+++ src/core/overseer/state.py
@@ -6,10 +6,62 @@ from ..game_elements.abstract_elements import Team
 
 @dataclass
 class State:
-    mode: str
-    turn: int
-    life: int
-    battle_lost: bool
-    team: Team
-    shop: Shop = None
-    gold: int = 10
+    _mode: str
+    _turn: int
+    _life: int
+    _gold: int
+    _battle_lost: bool
+    _team: Team
+    _shop: Shop = None
+
+    def __init__(self, mode: str, turn: int, life: int, gold: int,
+                 battle_lost: bool, team: Team, shop: Shop = None):
+        self._mode = mode
+        self._turn = turn
+        self._life = life
+        self._gold = gold
+        self._battle_lost = battle_lost
+        self._team = team
+        self._shop = shop
+
+    def __eq__(self, other: 'State') -> bool:
+        if type(other) != type(self):
+            return False
+        for key in other.__dict__:
+            if key in self.__dict__:
+                if self.__dict__[key] != other.__dict__[key]:
+                    return False
+            else:
+                return False
+        return True
+
+    @property
+    def mode(self) -> str:
+        return self._mode
+
+    @property
+    def turn(self) -> int:
+        return self._turn
+
+    @property
+    def life(self) -> int:
+        return self._life
+
+    @property
+    def gold(self) -> int:
+        return self._gold
+
+    @property
+    def battle_lost(self) -> bool:
+        return self._battle_lost
+
+    @property
+    def team(self) -> Team:
+        return self._team
+
+    @property
+    def shop(self) -> Shop:
+        if self._shop is None:
+            return Shop(self.mode, self.turn)
+        else:
+            return self._shop
