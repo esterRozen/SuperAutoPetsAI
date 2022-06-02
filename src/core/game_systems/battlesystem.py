6c875c15faca5bb8c4d979cfdf007b113c240fec
hunter <ianrozen@buffalo.edu>
2022-06-02 00:57:22 -0400

    working on actual loop process and adding handler positions for events.

--- src/core/game_systems/battlesystem.py
+++ src/core/game_systems/battlesystem.py
@@ -39,53 +39,41 @@ class BattleSystem:
         # start battle trigger
         # handles start of battle for both teams
         self._add_event(eventnames.START_BATTLE)
+        self.__agent.handle_events()
 
         # loop: (interleave team and enemy events)
 
-        # before attack (team)
-        # before attack (enemy)
+        while self.__agent.team.size > 0 and self.__agent.enemy.size > 0:
+            # before attack (team)
+            # before attack (enemy)
 
-        self._add_event(eventnames.BEFORE_ATTACK, event_raiser=("team", 0))
-        self._add_event(eventnames.BEFORE_ATTACK, event_raiser=("enemy", 0))
+            self._add_event(eventnames.BEFORE_ATTACK, event_raiser=("team", 0))
+            self._add_event(eventnames.BEFORE_ATTACK, event_raiser=("enemy", 0))
+            self.__agent.handle_events()
 
-        # attack (team)
-        # attack (enemy)
-        self._add_event(eventnames.ATTACK, event_raiser=("team", 0), target=("enemy", 0))
-        self._add_event(eventnames.ATTACK, event_raiser=("enemy", 0), target=("team", 0))
-        # friend ahead attacks (team)
-        # friend ahead attacks (enemy)
+            # attack (team)
+            # attack (enemy)
+            self._add_event(eventnames.ATTACK, event_raiser=("team", 0), target=("enemy", 0))
+            self._add_event(eventnames.ATTACK, event_raiser=("enemy", 0), target=("team", 0))
+            # friend ahead attacks (team)
+            # friend ahead attacks (enemy)
 
-        self._add_event(eventnames.FRIEND_AHEAD_ATTACKS, event_raiser=("team", 0))
-        self._add_event(eventnames.FRIEND_AHEAD_ATTACKS, event_raiser=("enemy", 0))
+            self._add_event(eventnames.FRIEND_AHEAD_ATTACKS, event_raiser=("team", 0))
+            self._add_event(eventnames.FRIEND_AHEAD_ATTACKS, event_raiser=("enemy", 0))
+            self.__agent.handle_events()
 
-        # if team unit still above 0 hp:
-        # hurt
+            # if team unit not above 0 hp:
+            # on faint (team)
+            # friend faints
+            # friend ahead faints
+            # knock out (on enemy team)
 
-        # if enemy unit still above 0 hp:
-        # hurt (enemy)
-
-        self.__er = ("team", 0)
-        self.__agent.handle_events()
-        self.__er = ("enemy", 0)
-        self.__agent.handle_events()
-
-        self.__er = ("enemy", 0)
-        self.__agent.handle_events()
-        self.__er = ("team", 0)
-        self.__agent.handle_events()
-
-        # if team unit not above 0 hp:
-        # on faint (team)
-        # friend faints
-        # friend ahead faints
-        # knock out (on enemy team)
-
-        # if enemy unit not above 0 hp:
-        # on faint (enemy)
-        # friend faints (enemy)
-        # friend ahead faints (enemy)
-        # knock out (on friendly team)
-        pass
+            # if enemy unit not above 0 hp:
+            # on faint (enemy)
+            # friend faints (enemy)
+            # friend ahead faints (enemy)
+            # knock out (on friendly team)
+            pass
 
     def summon(self, unit: Animal, target: Tuple[str, int]):
         self.__agent.team_of_(target).summon(unit, target[1])
