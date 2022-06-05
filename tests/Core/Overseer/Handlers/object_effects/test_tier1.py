4a2d5f9254e2cf574ba9fb3ead9d8e331bacdeef
hunter <ianrozen@buffalo.edu>
2022-06-05 18:45:15 -0400

    working on tier 1 effects tests

--- tests/Core/Overseer/Handlers/object_effects/test_tier1.py
+++ tests/Core/Overseer/Handlers/object_effects/test_tier1.py
@@ -1,14 +1,48 @@
 from unittest import TestCase
 
+from src.core.game_elements.game_objects.animals import tier_1
+from src.core.game_systems import ShopSystem, BattleSystem
+from src.core.overseer import MessageAgent
+
+from src.core.overseer.handlers.object_effects.tier1 import Tier1
+
 
 class TestTier1(TestCase):
+    def setUp(self) -> None:
+        self.agent = MessageAgent("base pack")
+        ShopSystem(self.agent)
+        BattleSystem(self.agent)
+
     def test__ant(self):
-        # TODO
-        self.fail()
+        self.agent.summon(tier_1.Bee(), ("team", 1))
+        ant = tier_1.Ant()
+
+        Tier1.ant(self.agent, ("team", 0), ("team", 4), ant)
+
+        self.assertTrue(self.agent.team[1].atk == 3)
+        self.assertTrue(self.agent.team[1].hp == 2)
+        self.assertTrue(self.agent.team[1].battle_atk == 3)
+        self.assertTrue(self.agent.team[1].battle_hp == 2)
+
+        ant.xp = 5
+        self.assertTrue(ant.level == 3)
+
+        Tier1.ant(self.agent, ("team", 0), ("team", 4), ant)
+
+        self.assertTrue(self.agent.team[1].atk == 9)
+        self.assertTrue(self.agent.team[1].hp == 5)
+        self.assertTrue(self.agent.team[1].battle_atk == 9)
+        self.assertTrue(self.agent.team[1].battle_hp == 5)
 
     def test__beaver(self):
-        # TODO
-        self.fail()
+        self.agent.summon(tier_1.Bee(), ("team", 1))
+        self.agent.summon(tier_1.Beaver(), ("team", 0))
+
+        Tier1.beaver(self.agent, ("team", 0), ("team", 3))
+        self.assertTrue(self.agent.team[1].atk == 1)
+        self.assertTrue(self.agent.team[1].hp == 2)
+        self.assertTrue(self.agent.team[1].battle_atk == 1)
+        self.assertTrue(self.agent.team[1].battle_hp == 2)
 
     def test__beetle(self):
         # TODO
@@ -47,9 +81,5 @@ class TestTier1(TestCase):
         self.fail()
 
     def test__pig(self):
-        # TODO
-        self.fail()
-
-    def test__sloth(self):
         # TODO
         self.fail()
