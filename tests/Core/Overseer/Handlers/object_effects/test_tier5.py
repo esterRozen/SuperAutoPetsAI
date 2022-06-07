4de4214a7fa6a28c3368c7e20c047ed8d31b8fe3
hunter <ianrozen@buffalo.edu>
2022-06-07 02:51:03 -0400

    starting some tier 5 tests

--- tests/Core/Overseer/Handlers/object_effects/test_tier5.py
+++ tests/Core/Overseer/Handlers/object_effects/test_tier5.py
@@ -1,7 +1,16 @@
 from unittest import TestCase
 
+from src.core.game_elements.game_objects.animals import tier_1, tier_2, tier_5
+from src.core.game_systems import BattleSystem, ShopSystem
+from src.core.overseer import MessageAgent
+
 
 class TestTier5(TestCase):
+    def setUp(self) -> None:
+        self.agent = MessageAgent("base pack")
+        BattleSystem(self.agent)
+        ShopSystem(self.agent)
+
     def test__chicken(self):
         # TODO
         self.fail()
@@ -28,6 +37,14 @@ class TestTier5(TestCase):
 
     def test__parrot(self):
         # TODO
+        self.fail()
+
+    def test__poodle(self):
+        self.agent.summon(tier_1.Fish(), ("team", 4))
+        self.agent.summon(tier_2.Shrimp(), ("team", 3))
+        self.agent.summon(tier_1.Fish(), ("team", 2))
+        self.agent.summon(tier_5.Poodle(), ("team", 1))
+        self.agent.summon(tier_5.Cow(), ("team", 0))
         self.fail()
 
     def test__rhino(self):
