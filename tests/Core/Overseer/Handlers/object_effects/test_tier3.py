1f00ca849d859194543564466dad2ebc34250e0a
hunter <ianrozen@buffalo.edu>
2022-06-06 21:00:27 -0400

    filling out tests for tier 3 effects

--- tests/Core/Overseer/Handlers/object_effects/test_tier3.py
+++ tests/Core/Overseer/Handlers/object_effects/test_tier3.py
@@ -1,20 +1,64 @@
 from unittest import TestCase
 
+from src.core.game_elements.abstract_elements import Empty
+from src.core.game_elements.game_objects.animals import tier_3
+from src.core.game_systems import BattleSystem, ShopSystem
+from src.core.overseer import MessageAgent
+from src.core.overseer.handlers.object_effects import Tier3
+
 
 class TestTier3(TestCase):
+    def setUp(self) -> None:
+        self.agent = MessageAgent("base pack")
+        BattleSystem(self.agent)
+        ShopSystem(self.agent)
+
     def test__badger(self):
-        # TODO
-        self.fail()
+        self.agent.in_shop = False
+
+        self.agent.summon(tier_3.Kangaroo(), ("team", 0))
+        badger = tier_3.Badger()
+        self.agent.summon(tier_3.Kangaroo(), ("team", 2))
+
+        Tier3.badger(self.agent, ("team", 1), ("team", 3), badger)
+
+        self.assertTrue(isinstance(self.agent.team[0], Empty))
+        self.assertTrue(isinstance(self.agent.team[2], Empty))
+
+        self.agent.summon(tier_3.Kangaroo(), ("team", 1))
+        self.agent.summon(tier_3.Kangaroo(), ("enemy", 0))
+
+        Tier3.badger(self.agent, ("team", 0), ("team", 3), badger)
+
+        self.assertTrue(isinstance(self.agent.team[1], Empty))
+        self.assertTrue(isinstance(self.agent.enemy[0], Empty))
+
+        self.agent.summon(tier_3.Giraffe(), ("team", 0))
+        self.agent.summon(tier_3.Giraffe(), ("team", 2))
+
+        Tier3.badger(self.agent, ("team", 1), ("team", 3), badger)
+
+        self.assertTrue(self.agent.team[0].battle_hp == 2)
+        self.assertTrue(self.agent.team[2].battle_hp == 2)
 
     def test__blowfish(self):
-        # TODO
-        self.fail()
+        self.agent.in_shop = False
+        self.agent.summon(tier_3.Blowfish(), ("team", 0))
+        self.agent.summon(tier_3.Giraffe(), ("enemy", 0))
+
+        Tier3.blowfish(self.agent, ("team", 0), ("team", 1))
+
+        self.assertTrue(self.agent.enemy[0].battle_hp == 2)
 
     def test__camel(self):
         # TODO
         self.fail()
 
     def test__caterpillar(self):
+        # TODO
+        self.fail()
+
+    def test__dog(self):
         # TODO
         self.fail()
 
