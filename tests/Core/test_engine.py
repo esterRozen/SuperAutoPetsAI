a033b7d6c013673bf741251a4b6c336564332789
hunter <ianrozen@buffalo.edu>
2022-06-10 18:22:10 -0400

    created test skeleton for engine

---
+++ tests/Core/test_engine.py
@@ -0,0 +1,37 @@
+from unittest import TestCase
+
+
+class TestEngine(TestCase):
+
+    def test_save(self):
+        self.fail()
+
+    def test_load(self):
+        self.fail()
+
+    def test_move(self):
+        self.fail()
+
+    def test_combine(self):
+        self.fail()
+
+    def test_sell(self):
+        self.fail()
+
+    def test_buy(self):
+        self.fail()
+
+    def test_freeze(self):
+        self.fail()
+
+    def test_reroll(self):
+        self.fail()
+
+    def test_end_turn(self):
+        self.fail()
+
+    def test_fight(self):
+        self.fail()
+
+    def test_start_turn(self):
+        self.fail()
