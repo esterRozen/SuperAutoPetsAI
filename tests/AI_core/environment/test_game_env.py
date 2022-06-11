220b7924e486d2d6dd711f2f950f0a981020df3a
hunter <ianrozen@buffalo.edu>
2022-06-10 23:59:54 -0400

    new test skeletons to figure stuff out

--- tests/AI_core/environment/test_game_env.py
+++ tests/AI_core/environment/test_game_env.py
@@ -1,7 +1,12 @@
 from unittest import TestCase
 
+from src.AI_core.environment import SAPGame
+
 
 class TestSAPGame(TestCase):
+    def setUp(self) -> None:
+        self.env = SAPGame("base pack")
+
     def test_step(self):
         self.fail()
 
@@ -9,4 +14,9 @@ class TestSAPGame(TestCase):
         self.fail()
 
     def test_render(self):
+        self.fail()
+
+    def test_sample(self):
+        action_sample = self.env.action_space.sample()
+        observation_sample = self.env.observation_space.sample()
         self.fail()
