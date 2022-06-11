a42596d303e2fc9d4d043b9e39fdccf93f64f01f
hunter <ianrozen@buffalo.edu>
2022-06-10 23:49:40 -0400

    setting up game env test skeleton

---
+++ tests/AI_core/environment/test_game_env.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+
+class TestSAPGame(TestCase):
+    def test_step(self):
+        self.fail()
+
+    def test_reset(self):
+        self.fail()
+
+    def test_render(self):
+        self.fail()
