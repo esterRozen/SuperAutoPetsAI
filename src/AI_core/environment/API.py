d68d3dfcb1f8703c46151c142550d8eaa3201920
hunter <ianrozen@buffalo.edu>
2022-06-10 23:03:17 -0400

    a little bit of mapping out api interfacing engine and AI environment

--- src/AI_core/environment/API.py
+++ src/AI_core/environment/API.py
@@ -1,12 +1,21 @@
-from typing import Tuple, Optional, Union
 from src.core import Engine
 
 __all__ = ['EngineAPI']
 
 
 class EngineAPI:
-    def __init__(self, mode):
+    def __init__(self, mode, limit: int = 50):
         self.__engine = Engine(mode)
 
+        self.__limit = 50
+        self.__actions_this_turn = 0
+
     def process_commands(self, command):
-        pass
+        """
+        Processes action state commands into engine readable commands
+        Args:
+            command:
+
+        Returns:
+
+        """
