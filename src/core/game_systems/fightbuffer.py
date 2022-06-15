25b76b78cdb5ca99e38f6fc7ad40822873557bde
hunter <ianrozen@buffalo.edu>
2022-06-14 21:51:48 -0400

    async fight buffer can now handle matchups for different turns.

--- src/core/game_systems/fightbuffer.py
+++ src/core/game_systems/fightbuffer.py
@@ -1,31 +1,33 @@
 from threading import Lock
 import random as rand
+from typing import Dict, List
+
 from ..game_elements.abstract_elements import Team
 from ..game_elements.game_objects.game_objects import MetaSingleton
 
 
 class FightBuffer(metaclass=MetaSingleton):
     def __init__(self, limit=500):
-        self._stored = []
-        self._size = 0
+        self._stored: Dict[int, List] = {}
+        self._size: Dict[int, int] = {turn: 0 for turn in range(1, 21)}
         self._limit = limit
         self._lock = Lock()
 
-    def push(self, team):
+    def push(self, team, turn: int):
         with self._lock:
-            if self._size < self._limit:
-                self._stored.append(team)
-                self._size += 1
+            if self._size[turn] < self._limit:
+                self._stored[turn].append(team)
+                self._size[turn] += 1
             else:
-                self._stored[rand.randrange(0, self._limit)] = team
+                self._stored[turn][rand.randrange(0, self._limit)] = team
 
-    def pop(self):
+    def pop(self, turn: int):
         with self._lock:
-            if self._size == 0:
+            if self._size[turn] == 0:
                 return Team()
 
-            if self._size == 5000:
+            if self._size[turn] == 5000:
                 self._size -= 1
-                return self._stored.pop(rand.randrange(0, self._size))
+                return self._stored[turn].pop(rand.randrange(0, self._size[turn]))
 
-            return self._stored[rand.randrange(0, self._size)]
+            return self._stored[turn][rand.randrange(0, self._size[turn])]
