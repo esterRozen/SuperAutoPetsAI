4b883cce2a95436e1d1d5103967a28065fde2a61
hunter <ianrozen@buffalo.edu>
2022-06-15 22:51:39 -0400

    progress, actions now happen with epsilon greedy. full episodes can be played. reworded train function

--- src/AI_core/Learners/DQN.py
+++ src/AI_core/Learners/DQN.py
@@ -1,24 +1,27 @@
+import random as rand
+from typing import Optional
+
 import keras
 import keras.layers as layers
 import numpy as np
 import tensorflow as tf
 
 from src.AI_core.environment import SAPGame
-from src.AI_core.replay.replayers import MultiChannelReplay
+from src.AI_core.replay.replayers import Transition, ReplayMemory
 
 _min_buffer = 50
 _num_actions = 99
 
 
 class DQN:
-    replay = MultiChannelReplay(20)
+    replay = ReplayMemory()
     env = SAPGame()
     gamma = 0.95
-    epsilon = 0.04
+    epsilon = 0.1
 
     def __init__(self):
         model = keras.Sequential()
-        model.add(layers.Input(shape=(61,)))
+        model.add(layers.Input(shape=(76,)))
         model.add(layers.Dense(40))
         model.add(layers.BatchNormalization())
 
@@ -37,19 +40,46 @@ class DQN:
         self.network = model
         self.optimizer = tf.optimizers.Adagrad()
 
-    def actions(self, observations):
-        return np.argmax(self.evaluate_forward(observations))
+    def write_weights_from(self, network: 'DQN'):
+        pass
 
-    def act(self, observations):
-        for observation in observations:
-            pass
+    def action(self, observation):
+        if rand.random() < self.epsilon:
+            return rand.randrange(0, _num_actions)
+        return np.argmax(self.evaluate_forward(observation))
+
+    def act(self, state, prev_reward):
+        action = self.action(state)
+        next_state, reward, done, info = self.env.step(action)
+        transition = Transition(state, action, prev_reward + reward, next_state, done)
+        self.replay.push(transition)
+        return transition
+
+    def play_episode(self, target: 'DQN', mode: Optional[str] = None):
+        i = 0
+        done = False
+        options = {"mode": mode}
+        state = self.env.reset(options=options)
+        sum_reward = 0
+        loss_list = []
+        while not done:
+            transition = self.act(state, sum_reward)
+            state = transition.next_state
+            done = transition.done
+            sum_reward = transition.reward
+
+            loss = self.train_based_on(target)
+            if loss is not None:
+                loss_list.append(loss)
+
+        return sum_reward, sum(loss_list) / len(loss_list)
 
     def evaluate_forward(self, inputs):
         return self.network(inputs)
 
-    def train(self, target: 'DQN'):
-        # if self.replay.__len__() < _min_buffer:
-        #     return
+    def train_based_on(self, target: 'DQN') -> Optional[tf.Tensor]:
+        if self.replay.__len__() < _min_buffer:
+            return None
 
         experiences = self.replay.sample_processed(32)
         state = experiences.state
@@ -61,7 +91,7 @@ class DQN:
         s_t1_values_pred = target.evaluate_forward(next_state)
         s_t1_values_actual = np.where(done, reward, reward + self.gamma * np.max(s_t1_values_pred, axis=1))
 
-        with tf.GradientTape as recorder:
+        with tf.GradientTape() as recorder:
             action_values = tf.math.reduce_sum(
                 self.evaluate_forward(state) * tf.one_hot(action, _num_actions),
                 axis=1)
@@ -74,7 +104,13 @@ class DQN:
 
 
 if __name__ == '__main__':
-    active = DQN()
-    target = DQN()
+    copy_frequency = 10
+    active_net = DQN()
+    target_net = DQN()
 
-    active.train(target)
+    for i in range(10000):
+        active_net.play_episode(target_net)
+        active_net.epsilon = 0.999 * active_net.epsilon
+
+        if i % copy_frequency == 0:
+            target_net.write_weights_from(active_net)
