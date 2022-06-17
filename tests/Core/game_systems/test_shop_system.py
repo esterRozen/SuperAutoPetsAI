78598e7fb1270af07293fc07fd29cb5342335b6f
hunter <ianrozen@buffalo.edu>
2022-06-16 22:09:32 -0400

    some basic tests of shop system

--- tests/Core/game_systems/test_shop_system.py
+++ tests/Core/game_systems/test_shop_system.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 
+from src.core.game_elements.game_objects.animals import Swan
 from src.core.game_systems import BattleSystem, ShopSystem
 from src.core.overseer import MessageAgent
 
@@ -7,24 +8,48 @@ from src.core.overseer import MessageAgent
 class TestShopSystem(TestCase):
     def setUp(self) -> None:
         self.agent = MessageAgent("base pack")
-        BattleSystem(self.agent)
-        ShopSystem(self.agent)
-
-    def test_instantiation(self):
-        # TODO
-        self.fail()
+        self.agent.debug_mode_no_handle_queue = False
+        self.battle_sys = BattleSystem(self.agent)
+        self.shop_sys = ShopSystem(self.agent)
 
     def test_start_turn(self):
-        # TODO
-        self.fail()
+        self.agent.gold = 4
+        self.agent.team[0] = Swan()
+        self.agent.team[0].temp_buff(1, 1)
+        self.agent.store_backup()
+        self.shop_sys.start_turn()
+
+        self.assertTrue(self.agent.gold == 11)
+        self.assertTrue(self.agent.team[0].battle_atk == 1)
+        self.assertTrue(self.agent.team[0].atk == 1)
+        self.assertTrue(self.agent.team[0].battle_hp == 3)
+        self.assertTrue(self.agent.team[0].hp == 3)
 
     def test_toggle_freeze(self):
-        # TODO
-        self.fail()
+        result = self.shop_sys.toggle_freeze(0)
+        self.assertTrue(result == 0)
+        result = self.shop_sys.toggle_freeze(3)
+        self.assertTrue(result == -1)
+        result = self.shop_sys.toggle_freeze(5)
+        self.assertTrue(result == 0)
+        result = self.shop_sys.toggle_freeze(6)
+        self.assertTrue(result == -1)
+
+        shop = self.agent.shop
+        self.assertTrue(shop[0].is_frozen)
+        self.assertTrue(not shop[3].is_frozen)
+        self.assertTrue(shop[5].is_frozen)
+        self.assertTrue(not shop[6].is_frozen)
 
     def test_reroll(self):
-        # TODO
-        self.fail()
+        item1 = self.agent.shop[0].item
+        item2 = self.agent.shop[1].item
+        self.shop_sys.reroll()
+        # equality checks if it's the same object instance.
+        self.assertTrue(self.agent.shop[0].item != item1)
+        self.assertTrue(self.agent.shop[1].item != item2)
+
+        self.assertTrue(self.agent.gold == 9)
 
     def test_buy(self):
         # TODO
