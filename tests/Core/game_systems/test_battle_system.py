3e29d578566cc4b50d50c3566e976f04d59d29a9
hunter <ianrozen@buffalo.edu>
2022-06-18 18:58:23 -0400

    tsting basic battles

--- tests/Core/game_systems/test_battle_system.py
+++ tests/Core/game_systems/test_battle_system.py
@@ -1,13 +1,28 @@
 from unittest import TestCase
 
+from src.core.game_elements.abstract_elements import Team
+from src.core.game_elements.game_objects.animals import tier_1
+from src.core.game_elements.game_objects.equipment import Honey
+from src.core.game_systems import BattleSystem, ShopSystem
+from src.core.overseer import MessageAgent
+
 
 class TestBattleSystem(TestCase):
-    def test_instantiation(self):
-        # TODO
-        self.fail()
+
+    def setUp(self) -> None:
+        self.agent = MessageAgent("base pack")
+        self.battle_sys = BattleSystem(self.agent)
+        ShopSystem(self.agent)
+        self.agent.debug_mode_no_handle_queue = False
+        self.agent.in_shop = False
 
     def test_start_battle(self):
-        # TODO
+        self.agent.team[0] = tier_1.Fish()
+        self.agent.team[0].held = Honey()
+        enemy = Team()
+        enemy[0] = tier_1.Cricket()
+
+        self.battle_sys.start_battle(enemy)
         self.fail()
 
     def test_summon(self):
