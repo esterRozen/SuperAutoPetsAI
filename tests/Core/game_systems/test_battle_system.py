fe268752a9ae0ba97e97eb23883ce56530cee740
hunter <ianrozen@buffalo.edu>
2022-06-19 21:18:40 -0400

    working on validating more complex battles

--- tests/Core/game_systems/test_battle_system.py
+++ tests/Core/game_systems/test_battle_system.py
@@ -1,8 +1,8 @@
 from unittest import TestCase
 
 from src.core.game_elements.abstract_elements import Team
-from src.core.game_elements.game_objects.animals import tier_1
-from src.core.game_elements.game_objects.equipment import Honey
+from src.core.game_elements.game_objects.animals import tier_1, tier_2, tier_3, tier_4, tier_5
+from src.core.game_elements.game_objects.equipment import Honey, Garlic
 from src.core.game_systems import BattleSystem, ShopSystem
 from src.core.overseer import MessageAgent
 
@@ -16,7 +16,7 @@ class TestBattleSystem(TestCase):
         self.agent.debug_mode_no_handle_queue = False
         self.agent.in_shop = False
 
-    def test_start_battle(self):
+    def test_basic_battle(self):
         self.agent.team[0] = tier_1.Fish()
         self.agent.team[0].held = Honey()
         enemy = Team()
@@ -25,6 +25,24 @@ class TestBattleSystem(TestCase):
         self.battle_sys.start_battle(enemy)
         self.assertTrue(self.agent.wins == 1)
         self.assertTrue(isinstance(self.agent.team[0], tier_1.Bee))
+
+    def test_complex_battle(self):
+        self.agent.team[0] = tier_2.Hedgehog()
+        self.agent.team[1] = tier_3.Camel()
+        self.agent.team[1].held = Garlic()
+        self.agent.team[2] = tier_4.Skunk()
+        self.agent.team[2].held = Garlic()
+        self.agent.team[3] = tier_3.Sheep()
+        self.agent.team[4] = tier_3.Badger()
+        self.agent.team[4].held = Honey()
+
+        enemy = Team()
+        enemy[0] = tier_5.Eagle()
+        enemy[1] = tier_3.Kangaroo()
+        enemy[2] = tier_2.Spider()
+        enemy[3] = tier_1.Otter()
+        enemy[4] = tier_4.Deer()
+        self.battle_sys.start_battle(enemy)
 
     def test_summon(self):
         # TODO
